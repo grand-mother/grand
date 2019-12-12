4fed6565dec13c1b0feb686da7389339cb540990
Valentin Niess <niess@in2p3.fr>
2019-12-12 16:32:10 +0100

    patching CoreasInfoFunctions

--- lib/python/grand/radio/CoreasInfoFunctions.py
+++ lib/python/grand/radio/CoreasInfoFunctions.py
@@ -24,11 +24,11 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
 #===========================================================================================================
     '''
     Get shower parameter from inp and reas file for coreas simulations
-    
+
     Parameters:
         input_file_path: str
             path of inp file
-        
+
     Returns:
         zen: float
             zenith, deg in GRAND conv.
@@ -45,14 +45,14 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
         task: str
             ID of shower
     '''
-    
-    
-    if os.path.isfile(input_file_path) ==  False:  # File does not exist 
+
+
+    if os.path.isfile(input_file_path) ==  False:  # File does not exist
         print('--- ATTENTION: inp-file does not exist')
         exit()
-        
+
     #datafile = file(input_file_path) # why it is not working...
-    datafile = open(input_file_path, 'r') 
+    datafile = open(input_file_path, 'r')
 
     for line in datafile:
         # NOTE: CORSIKA/CoREAS angle = direction of propgation == GRAND conventions
@@ -91,7 +91,7 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
         injh
     except NameError:
         injh = 100000.e2*u.m #Case of a cosmic for which no injection height is defined in the input file and is then set to 100 km in cm
-            
+
     # Get reas file
     path, reas = os.path.split(input_file_path)
     base = os.path.basename(reas)
@@ -99,7 +99,7 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
     file_path= path[0:-4]+'/'+base1[0]+".info"
     #file_path= path[0:-4]+'/'+base1[0]+".reas"
 
-    datafile = open(file_path, 'r') 
+    datafile = open(file_path, 'r')
     for line in datafile:
         if 'TASK' in line:
             task = str(line.split('  ',-1)[1])
@@ -107,12 +107,12 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
                 task=task[0:-1]
             if task[-1]=='\r':
                 task=task[0:-1]
-            
+
         if 'CORE' in line:
             #print(line)
             offset = line.split('  ',-1)
             offset[-1]=offset[-1].rstrip()
-            core = list([float(offset[1]), float(offset[2]), float(offset[3])])*u.m # in cm to m 
+            core = list([float(offset[1]), float(offset[2]), float(offset[3])])*u.m # in cm to m
     try:
         task
     except NameError:
@@ -120,9 +120,9 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
     try:
         core
     except NameError:
-        core = None       
-        
-        
+        core = None
+
+
     if task:
         if core is not None:
             return zen,azim,energy,injh,primarytype,core,task
@@ -135,27 +135,27 @@ def inputfromtxt_coreas(input_file_path): # still ongoing work
 def _get_positions_coreas(path):
     '''
     read in antenna positions from Coreas simulations, wrt to sealevel
-    
+
     Parameters:
     datafile: str
         path to folder of run
-    
+
     Returns:
     positions: numpy array
         x,y,z component of antenna positions in meters
     ID_ant: list
         corresponding antenna ID for identification !- [0,1,2,....]
-        
-        
-    NOTE: units assign to positions and slope assuming meters and degree 
+
+
+    NOTE: units assign to positions and slope assuming meters and degree
     '''
-    datafile = open(path, 'r') 
+    datafile = open(path, 'r')
     x_pos1=[]
     y_pos1=[]
     z_pos1=[]
     ID_ant=[]
     #positions=[]
-    
+
     alpha=[]
     beta=[]
     for line in datafile:
@@ -169,21 +169,21 @@ def _get_positions_coreas(path):
             alpha.append(0)
             beta.append(0)
         if 'ANTENNA' in line: #info file
-            x_pos1.append(float(line.split('  ',-1)[2])) #*u.m) 
-            y_pos1.append(float(line.split('  ',-1)[3])) #*u.m) 
-            z_pos1.append(float(line.split('  ',-1)[4])) #*u.m) 
+            x_pos1.append(float(line.split('  ',-1)[2])) #*u.m)
+            y_pos1.append(float(line.split('  ',-1)[3])) #*u.m)
+            z_pos1.append(float(line.split('  ',-1)[4])) #*u.m)
             ID_ant.append(str(line.split('  ',-1)[1]))
             alpha.append(float(line.split('  ',-1)[5]))
             beta.append(float(line.split('  ',-1)[6]))
-            
-            
+
+
     x_pos1=np.asarray(x_pos1)
     y_pos1=np.asarray(y_pos1)
     z_pos1=np.asarray(z_pos1)
     positions=np.stack((x_pos1,y_pos1, z_pos1), axis=-1 )*u.m
     slopes=np.stack((alpha, beta), axis=-1 )*u.deg
-    #print(ID_ant)    
-    
+    #print(ID_ant)
+
     return positions, ID_ant, slopes
 
 #===========================================================================================================
@@ -191,16 +191,16 @@ def _get_positions_coreas(path):
 def get_Xmax_coreas(path):
     """ read Xmax value from simulations, inspred by Rio group
         --- work in progress
-        
+
     Parameters:
         path: str
             path to folder of run
-    
+
     Returns:
-        Xmax: float 
+        Xmax: float
             slant depth in g/cm^2
     """
-    
+
     simInput_file = glob.glob(path+"/*.reas")[0]
 
     try:
@@ -219,18 +219,18 @@ def get_Xmax_coreas(path):
 def _get_distanceXmax_coreas(path):
     '''
     Reads the distance to Xmax from reas file , function from Rio group
-    
+
     Parameters:
         path: str
             path to folder of run
-    
+
     Returns:
-        d_Xmax: float 
+        d_Xmax: float
             distance to Xmax in m
     '''
 
     simInput_file = glob.glob(path+"/*.reas")[0]
-    
+
     try:
         fp = open(simInput_file,'r')
         for line in fp:
@@ -239,7 +239,7 @@ def _get_distanceXmax_coreas(path):
         fp.close()
     except:
         d_Xmax = None
-        
+
     return d_Xmax
 
 #===========================================================================================================
@@ -247,19 +247,19 @@ def _get_distanceXmax_coreas(path):
 def _get_showercore_coreas(path):
     '''
     Reads the distance to Xmax from reas file, inspired by Rio group
-    
+
     Parameters:
         path: str
             path to folder of run
-    
+
     Returns:
-        numpy array 
+        numpy array
             shower core in m
     '''
 
     simInput_file = glob.glob(path+"/*.reas")[0]
 
-    
+
     try:
         fp = open(simInput_file,'r')
         for line in fp:
@@ -270,14 +270,9 @@ def _get_showercore_coreas(path):
             if 'CoreCoordinateVertical' in line:
                 CoreZ = float(line.split()[2])/100
             fp.close()
-            
+
         return np.array([CoreX,CoreY,CoreZ])
-        
+
     except:
         logger.debug('Shower core not found')
         return None
-        
-    
-    
-    
-        
