452bf261a263191b8f4f6ca156b34fa45c008d42
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-11-04 12:13:16 +0100

    add check type with mypy

--- quality/grand_quality_type.bash
+++ quality/grand_quality_type.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd $GRAND_ROOT
-pylint grand --rcfile quality/pylint.conf -r y  --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee quality/report_pylint.txt
-
+mkdir -p user/grand/stubs
+MYPYPATH=user/grand/stubs
+mypy --config-file=tests/mypy.ini --pretty --html-report quality/html_mypy -p grand > quality/report_type.txt
