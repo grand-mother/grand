69a6c7a12faf2d7968f677cc31ef861be1d2fe28
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-11-04 14:59:29 +0100

    change name script, need to add --source option

--- quality/grand_quality_test_cov.bash
+++ quality/grand_quality_test_cov.bash
@@ -2,8 +2,9 @@
 
 cd $GRAND_ROOT
 coverage erase
-rm -f quality/report_*.*
-coverage run -m pytest tests 
+rm -f quality/report_coverage*
+rm -rf quality/html_cov*
+coverage run --source=grand -m pytest tests 
 coverage xml -i -o quality/report_coverage.xml
 coverage html -i -d quality/html_coverage
-
+coverage report
