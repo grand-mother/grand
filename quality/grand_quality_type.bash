ba1922e46175b7b34777a953ae94521c69c1fe2b
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-11-05 12:49:25 +0100

    print report at the end

--- quality/grand_quality_type.bash
+++ quality/grand_quality_type.bash
@@ -4,3 +4,5 @@ cd $GRAND_ROOT
 mkdir -p user/grand/stubs
 MYPYPATH=user/grand/stubs
 mypy --config-file=tests/mypy.ini --pretty --html-report quality/html_mypy -p grand > quality/report_type.txt
+echo "================================== check type : mypy report"
+cat quality/report_type.txt
