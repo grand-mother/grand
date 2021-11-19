2db21d7b01cd3d56a4282c201b4157a41910fc0c
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-11-19 18:13:42 +0100

    just to test ci is available

--- quality/grand_quality_analysis.bash
+++ quality/grand_quality_analysis.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $GRAND_ROOT
-pylint grand --rcfile quality/pylint.conf -r y  --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee quality/report_pylint.txt
+pylint grand --rcfile quality/pylint.conf -r y --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee quality/report_pylint.txt
 
