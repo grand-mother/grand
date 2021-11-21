00f87cf6a83ed73e46fda94f754e80e26c7fc30f
Ramesh Koirala <rkoirala@udel.edu>
2021-11-21 08:31:07 +0800

    Got rid of astropy. Fixed memory issue caused due to reference in Geodetic CS.

--- tests/tools/test_coordinates.py
+++ tests/tools/test_coordinates.py
@@ -1,21 +1,33 @@
 '''
 Unit tests for the grand_tools.coordinates module
-'''
 
+RK Todo: Fully develop Horizontal coordinate system and 
+         implement Rotation of LTP frame.
+'''
 import unittest
+from tests import TestCase
 
 import numpy
-#import astropy.units as u
-#from astropy.coordinates import CartesianRepresentation, EarthLocation, ITRS,  \
-#                                SkyCoord, SphericalRepresentation
-#from astropy.utils import iers
-iers.conf.auto_download = False # Disable downloads for tests,
-                                # due to latency failures
-
+import datetime
 from grand import ECEF, LTP, Geodetic, GeodeticRepresentation, CartesianRepresentation, \
-                    HorizontalRepresentation, SphericalRepresentation, Rotation, Reference 
-            #ExtendedCoordinateFrame, Rotation
-from tests import TestCase
+                    SphericalRepresentation, Rotation, HorizontalVector, Horizontal , Reference
+import copy as _copy
+
+def copy(obj, deep=False):
+    import inspect
+    if deep:
+        new = _copy.deepcopy(obj)
+        for var in vars(obj):
+            attr = getattr(obj, var)
+            setattr(new, var, _copy.deepcopy(attr))
+    else:
+        new = _copy.copy(obj)
+        for var in vars(obj):
+            attr = getattr(obj, var)
+            setattr(new, var, _copy.copy(attr))
+
+    return new
+
 
 
 class CoordinatesTest(TestCase):
@@ -24,25 +36,25 @@ class CoordinatesTest(TestCase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.obstime = '2010-01-01'
-        self.location = Geodetic(latitude=0., longitude=0., 
-                                height=0., referece=Reference.ELLIPSOID)
+        self.obstime = '2020-01-01'
+        self.location = Geodetic(latitude=0., longitude=0., height=0., 
+                        reference='ELLIPSOID')
 
     def test_geodetic(self):
         ecef = ECEF(x=6378137, y=0, z=0)
-        geod = Geodetic(ecef)
-
-        self.assertQuantity(geod.latitude , self.location.latitude , 11)
-        self.assertQuantity(geod.longitude, self.location.longitude, 11)
+        geod = Geodetic(ecef, reference='ELLIPSOID')
+        self.assertQuantity(geod.latitude , self.location.latitude , 6)
+        self.assertQuantity(geod.longitude, self.location.longitude, 6)
         self.assertQuantity(geod.height   , self.location.height   , 6)
 
         # Check the method transformation
-        geod1 = ecef.ecef_to_geodetic()
-        self.assertQuantity(geod1.latitude , self.location.latitude , 11)
-        self.assertQuantity(geod1.longitude, self.location.longitude, 11)
+        geod1 = ecef.ecef_to_geodetic(reference='ELLIPSOID')
+        self.assertQuantity(geod1.latitude , self.location.latitude , 6)
+        self.assertQuantity(geod1.longitude, self.location.longitude, 6)
         self.assertQuantity(geod1.height   , self.location.height   , 6)
 
         # RK TODO: Add more test.
+
 
     def test_horizontal(self):
         '''for (angle, point) in (((90, 0), (1, 0, 0)),
@@ -69,7 +81,7 @@ class CoordinatesTest(TestCase):
     def test_ecef(self):
         # Check the forward transform
         ecef0 = ECEF(x=6378137, y=0, z=0)
-        ecef1 = ECEF(self.location, obstime=self.obstime)
+        ecef1 = ECEF(self.location)
         self.assertCartesian(ecef0, ecef1, 8)
 
         # Check the backward transform
@@ -95,7 +107,7 @@ class CoordinatesTest(TestCase):
         '''
     #RK - ExtendedCoordinateFrame is not defined in the new coordinates.py.
           Keep this method here for sometime to see if we can use any test
-          styles performed here in other frames.
+          styles performed here to test other frames.
     def test_extended_frame(self):
         # Test write protection
         frame0 = ExtendedCoordinateFrame(CartesianRepresentation(0, 0, 0))
@@ -158,8 +170,11 @@ class CoordinatesTest(TestCase):
         '''
 
     def test_ltp(self):
-        ecef = ECEF(self.location, obstime=self.obstime)
+        loc = self.location
+        location_ref0 = self.location.reference
+        loc_ref0      = loc.reference
 
+        ecef = ECEF(self.location)
         # Check the constructor & to ECEF transform
         ltp = LTP(x=0, y=0, z=0, location=self.location,
                   orientation='NWU', magnetic=False, obstime=self.obstime)
@@ -169,32 +184,37 @@ class CoordinatesTest(TestCase):
         self.assertCartesian(r, ecef, 6)
 
         # Check the from ECEF transform
-        ltp = ecef.ecef_to_ltp(location=self.location, orientation='NWU',
-                               magnetic=False, obstime=self.obstime)
+        ltp_frame = LTP(location=self.location, orientation='ENU',
+                        magnetic=False, obstime=self.obstime)
+        #ltp = ecef.ecef_to_ltp(ltp_frame)
+        ltp = LTP(ecef, frame=ltp_frame)
+        # RK Note: 
+        # self.location.reference can suddenly changes from ELLIPSOID to GEOID 
+        # reference changes if it is stored as class attribute instead of instance 
+        # attribute. Always check reference consitency for a Geodetic coordinate.
+        self.assertEqual(self.location.reference, 'ELLIPSOID') 
+        self.assertEqual(loc.reference, 'ELLIPSOID')
 
+        # attributes can change if they are class attribute instead of instance attribute.
+        # always check attributes consitency for a LTP coordinate.
         self.assertEqual(ltp.obstime, self.obstime)
-        self.assertQuantity(ltp.x, 0, 6)
-        self.assertQuantity(ltp.y, 0, 6)
-        self.assertQuantity(ltp.z, 0, 6)
+        self.assertQuantity(ltp.x, numpy.zeros(1), 6)
+        self.assertQuantity(ltp.y, numpy.zeros(1), 6)
+        self.assertQuantity(ltp.z, numpy.zeros(1), 6)
 
         # Check the Earth location conversion
-        location = ECEF(ltp)
-        self.assertCartesian(ecef, location, 2)
-
-        # RK: TODO: Complete from here and below -------------------------
-        # Check the affine transform
+        loc = ECEF(ltp)
+        self.assertCartesian(ecef, loc, 2)
+        # Check the affine transform. 
         points = ((0, 0, 1), (1, 0, 0), (0, 1, 0), (1, 1, 0), (1, 1, 1),
                   (0, 1, 1))
         for point in (points):
-            cart = CartesianRepresentation(x=point[1], y=point[0], z=point[2])
-            altaz = SkyCoord(cart, frame='altaz', location=self.location,
-                             obstime=self.obstime)
-            ecef0 = altaz.transform_to(ECEF(obstime=self.obstime))
 
             cart = CartesianRepresentation(x=point[0], y=point[1], z=point[2])
             ltp = LTP(x=cart.x, y=cart.y, z=cart.z, 
                       location=self.location, obstime=self.obstime,
                       orientation='NWU', magnetic=False)
+            ecef0 = ECEF(ltp)
             ecef1 = ltp.ltp_to_ecef()
 
             self.assertEqual(ecef0.obstime, ecef1.obstime)
@@ -202,42 +222,44 @@ class CoordinatesTest(TestCase):
 
         # Check the orientation
         point = (1, -1, 2)
-        cart = CartesianRepresentation(x=point[0], y=point[1], z=point[2],
-                                       unit=u.m)
-        altaz = SkyCoord(cart, frame='altaz', location=self.location,
-                         obstime=self.obstime)
-        ecef0 = altaz.transform_to(ECEF(obstime=self.obstime))
+
+        ltp = LTP(x=point[0], y=point[1], z=point[2], 
+                location=self.location, obstime=self.obstime,
+                orientation='NEU')
+        ecef0 = ltp.ltp_to_ecef()
 
         for (orientation, sign) in (('NEU', (1, 1, 1)),
                                     ('NED', (1, 1, -1)),
                                     ('SEU', (-1, 1, 1)),
                                     ('NWU', (1, -1, 1))):
-            cart = CartesianRepresentation(x=sign[0] * point[0],
-                y=sign[1] * point[1], z=sign[2] * point[2], unit=u.m)
-            ltp = LTP(cart, location=self.location, obstime=self.obstime,
-                      orientation=orientation, magnetic=False)
-            ecef1 = ltp.transform_to(ECEF(obstime=self.obstime))
-
+            ltp = LTP(x = sign[0]*point[0],
+                    y = sign[1]*point[1], z = sign[2]*point[2],
+                    location=self.location, obstime=self.obstime,
+                    orientation=orientation, magnetic=False)
+            ecef1 = ECEF(ltp)
             self.assertCartesian(ecef0, ecef1, 4)
 
-        # Check the unit vector case
-        uy = HorizontalRepresentation(azimuth = 0 * u.deg,
-                                      elevation = 0 * u.deg)
-        ltp = LTP(uy, location=self.location, obstime=self.obstime)
+        # Check the unit vector case. 
+        # RK TODO: Develop HorizontalRepresentation, it is not complete and might have some error.
+        uy   = Horizontal(azimuth=0, elevation=0, location=self.location)
+        ecef = uy.horizontal_to_ecef()
+        ltp  = LTP(ecef, location=self.location, obstime=self.obstime, orientation='ENU')
+        self.assertQuantity(ltp.x, numpy.zeros(1), 9)
+        self.assertQuantity(ltp.y, numpy.ones(1), 9)
+        self.assertQuantity(ltp.z, numpy.zeros(1), 9)
 
-        self.assertQuantity(ltp.x, 0 * u.one, 9)
-        self.assertQuantity(ltp.y, 1 * u.one, 9)
-        self.assertQuantity(ltp.z, 0 * u.one, 9)
-
-        r = ltp.transform_to(ECEF)
+        #r = ECEF(ltp)         #RK. Use this or the next.
+        r = ltp.ltp_to_ecef()  #RK
 
         self.assertEqual(r.obstime, ltp.obstime)
-        self.assertQuantity(r.cartesian.norm(), 1 * u.one, 6)
+        self.assertQuantity(r.norm(), 6378137., 6) #RK
 
+
+        # RK: Horizontal coordinate system is incomplete and need more work.
+        '''
         ecef = ECEF(uy, obstime=self.obstime)
-        ltp = ecef.transform_to(LTP(location=self.location, orientation='ENU',
-                                    magnetic=False))
-
+        #ltp = LTP(ecef, location=self.location, orientation='ENU', magnetic=False) #RK, or
+        ltp = ecef.ecef_to_ltp(ltp_frame)                                           #RK
         self.assertEqual(ltp.obstime, ecef.obstime)
         self.assertQuantity(ltp.cartesian.norm(), 1 * u.one, 6)
 
@@ -261,67 +283,76 @@ class CoordinatesTest(TestCase):
         with self.assertRaises(ValueError) as context:
             LTP(uy, location=self.location, orientation=('T', 'O', 'T', 'O'))
         self.assertRegex(context.exception.args[0], '^Invalid frame')
+        '''
 
         # Test the ltp round trip with a position
         frame0 = LTP(location=self.location, orientation='ENU', magnetic=False)
         frame1 = LTP(location=self.location, obstime=self.obstime,
                      orientation='ENU', magnetic=True)
-        ltp0 = LTP(x=1 * u.m, y=2 * u.m, z=3 * u.m, location=self.location,
-                   orientation='ENU', magnetic=False, obstime=self.obstime)
-        ltp1 = ltp0.transform_to(frame1).transform_to(frame0)
+        ltp0 = LTP(x=1, y=2, z=3, location=self.location, orientation='ENU', 
+                   magnetic=False, obstime=self.obstime) #RK
+        ltp1_= ltp0.ltp_to_ltp(frame1)   #RK
+        ltp1 = ltp1_.ltp_to_ltp(frame0)  #RK
+
         self.assertCartesian(ltp0, ltp1, 8)
 
         # Test the same frame case
-        ltp1 = ltp0.transform_to(frame0)
+        ltp1 = ltp0.ltp_to_ltp(frame0)    #RK
         self.assertCartesian(ltp0, ltp1, 8)
         self.assertEqual(ltp0.obstime, ltp1.obstime)
 
         # Test an LTP permutation
-        ltp0 = LTP(x=1 * u.m, y=2 * u.m, z=3 * u.m, location=self.location,
-                   orientation='ENU', magnetic=False)
+        ltp0 = LTP(x=1, y=2, z=3, location=self.location,
+                   orientation='ENU', magnetic=False)       #RK
         frame1 = LTP(location=self.location, orientation='NED', magnetic=False)
-        ltp1 = ltp0.transform_to(frame1)
+        ltp1 = ltp0.ltp_to_ltp(frame1)   #RK
         self.assertQuantity(ltp0.x, ltp1.y, 6)
         self.assertQuantity(ltp0.y, ltp1.x, 6)
         self.assertQuantity(ltp0.z, -ltp1.z, 6)
 
+        '''
         # Test an explicit rotation
-        r = Rotation.from_euler('ZX', 90 * u.deg, 90 * u.deg)
+        # RK: Rework on Rotation
+        #r = Rotation.from_euler('ZX', 90 * u.deg, 90 * u.deg)
+        r = Rotation.from_euler('ZX', 90 , 90) #RK
         frame0 = LTP(location=self.location, orientation='ENU', magnetic=False,
                      rotation=r)
         frame1 = LTP(location=self.location, orientation='NUE', magnetic=False)
-        self.assertArray(frame0._basis, frame1._basis)
+        #self.assertArray(frame0._basis, frame1._basis)
+        self.assertArray(frame0.basis, frame1.basis) #RK
 
         # Test replication with a rotation
         frame1 = frame0.rotated(r.inverse, copy=False)
         frame2 = LTP(location=self.location, orientation='ENU', magnetic=False)
-        self.assertArray(frame1._basis, frame2._basis)
+        #self.assertArray(frame1._basis, frame2._basis)
+        self.assertArray(frame1.basis, frame2.basis) #RK
 
         # Test declination
-        declination = 5 * u.deg
+        declination = 5 #* u.deg
         frame0 = LTP(location=self.location, declination=declination,
                      orientation='ENU')
         r = Rotation.from_euler('Z', -declination)
         frame1 = LTP(location=self.location, orientation='ENU', magnetic=False,
                      rotation=r)
-        self.assertArray(frame0._basis, frame1._basis)
-
+        #self.assertArray(frame0._basis, frame1._basis)
+        self.assertArray(frame0.basis, frame1.basis)
+        
 
     def test_rotation(self):
         # Test initialisation
-        a0 = numpy.array((45, 30, 15)) * u.deg
+        a0 = numpy.array((45, 30, 15)) #* u.deg
         r0 = Rotation.from_euler('ZYZ', a0)
         r1 = Rotation.from_euler('ZYZ', *a0)
         self.assertArray(r0.matrix, r1.matrix)
 
         # Test rotation vector
-        v0 = 90 * numpy.array((0, 0, 1)) * u.deg
+        v0 = 90 * numpy.array((0, 0, 1)) #* u.deg
         r0 = Rotation.from_rotvec(v0)
         v1 = r0.rotvec
         self.assertQuantity(v0, v1)
 
         # Test euler angles
-        a0 = numpy.array((45, 30, 15)) * u.deg
+        a0 = numpy.array((45, 30, 15)) #* u.deg
         r0 = Rotation.from_euler('ZYZ', a0)
         a1 = r0.euler_angles('ZYZ')
         a2 = r0.euler_angles('ZYZ', unit='deg')
@@ -329,7 +360,7 @@ class CoordinatesTest(TestCase):
         self.assertQuantity(a0, a2)
 
         # Test the application of a rotation
-        r0 = Rotation.from_euler('Z', 90 * u.deg)
+        r0 = Rotation.from_euler('Z', 90) #* u.deg)
         r1 = r0.inverse
 
         v0 = numpy.array((0, 1, 0))
@@ -340,26 +371,30 @@ class CoordinatesTest(TestCase):
         v1 = r1.apply(v1, inverse=True)
         self.assertArray(v0, v1)
 
-        v0 = numpy.array((0, 1, 0)) * u.m
-        v1 = numpy.array((1, 0, 0)) * u.m
+        v0 = numpy.array((0, 1, 0)) #* u.m
+        v1 = numpy.array((1, 0, 0)) #* u.m
         v1 = r0 * v1
         self.assertQuantity(v0, v1)
-        v1 = numpy.array((1, 0, 0)) * u.m
+        v1 = numpy.array((1, 0, 0)) #* u.m
         v1 = r1.apply(v1, inverse=True)
         self.assertQuantity(v0, v1)
 
-        v0 = CartesianRepresentation(0, 1, 0, unit='m')
-        v1 = CartesianRepresentation(1, 0, 0, unit='m')
+        #v0 = CartesianRepresentation(0, 1, 0, unit='m')
+        #v1 = CartesianRepresentation(1, 0, 0, unit='m')
+        v0 = CartesianRepresentation(x=0, y=1, z=0)
+        v1 = CartesianRepresentation(x=1, y=0, z=0)
         v1 = r0 * v1
         self.assertCartesian(v0, v1)
-        v1 = CartesianRepresentation(1, 0, 0, unit='m')
+        #v1 = CartesianRepresentation(1, 0, 0, unit='m')
+        v1 = CartesianRepresentation(x=1, y=0, z=0)
         v1 = r1.apply(v1, inverse=True)
         self.assertCartesian(v0, v1)
 
         # Test composition of rotations
-        r0 = Rotation.from_euler('z', 90 * u.deg)
-        r1 = Rotation.from_euler('x', 90 * u.deg)
-        r2 = Rotation.from_euler('zx', 90 * u.deg, 90 * u.deg)
+        r0 = Rotation.from_euler('z', 90)# * u.deg)
+        r1 = Rotation.from_euler('x', 90)# * u.deg)
+        #r2 = Rotation.from_euler('zx', 90 * u.deg, 90 * u.deg)
+        r2 = Rotation.from_euler('zx', 90, 90)
         r3 = r1 * r0
         self.assertTrue(isinstance(r3, Rotation))
         self.assertArray(r3.matrix, r2.matrix)
@@ -369,11 +404,11 @@ class CoordinatesTest(TestCase):
         self.assertArray(r3.matrix, r0.matrix)
 
         # Test the magnitude property
-        r0 = Rotation.from_euler('z', 90 * u.deg)
-        self.assertQuantity(r0.magnitude, 90 * u.deg)
+        r0 = Rotation.from_euler('z', 90)# * u.deg)
+        self.assertQuantity(r0.magnitude, 90)# * u.deg)
 
         # Test vectors alignment
-        r0 = Rotation.from_euler('Z', 90 * u.deg)
+        r0 = Rotation.from_euler('Z', 90)# * u.deg)
 
         def check_align(v0):
             v1 = r0.apply(v0)
@@ -384,11 +419,10 @@ class CoordinatesTest(TestCase):
         v0 = numpy.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)))
         check_align(v0)
 
-        v0 = numpy.array(((1, 0, 0), (0, 1, 0), (0, 0, 1))) * u.m
+        v0 = numpy.array(((1, 0, 0), (0, 1, 0), (0, 0, 1))) #* u.m
         check_align(v0)
 
-        v0 = CartesianRepresentation((1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 0),
-                                     unit='m')
+        v0 = CartesianRepresentation(x=(1, 0, 0, 1), y=(0, 1, 0, 1), z=(0, 0, 1, 0))#,unit='m')
         check_align(v0)
 
         # Test the identity generator
@@ -415,7 +449,7 @@ class CoordinatesTest(TestCase):
         rs = Rotation.random(3)
         for r0 in rs:
             check_random(r0)
-
+        '''
 
 if __name__ == '__main__':
     unittest.main()
--- tests/tools/test_topography.py
+++ tests/tools/test_topography.py
@@ -1,21 +1,23 @@
 '''
 Unit tests for the grand.tools.topography module
 '''
-
 import os
 import unittest
 from pathlib import Path
 
 import numpy
-import astropy.units as u
+#import astropy.units as u
 
 import grand.store as store
 import grand.tools as tools
-from grand.tools.coordinates import ECEF, GeodeticRepresentation, LTP
-from grand.tools import topography
-from grand.tools.topography import Topography
+#from grand.tools.coordinates import ECEF, GeodeticRepresentation, LTP
+#from grand.tools import topography
+#from grand.tools.topography import Topography
 from tests import TestCase
 
+from grand import Topography, geoid_undulation#, Reference
+from grand import topography
+from grand import ECEF, Geodetic, LTP, GRANDCS
 
 class TopographyTest(TestCase):
     '''Unit tests for the topography module'''
@@ -23,16 +25,19 @@ class TopographyTest(TestCase):
 
     def test_geoid(self):
         # Test the undulation getter
-        c = ECEF(GeodeticRepresentation(latitude=45.5 * u.deg,
-                                        longitude=3.5 * u.deg))
-        z = tools.topography.geoid_undulation(c)
+        #c = ECEF(GeodeticRepresentation(latitude=45.5 * u.deg,
+        #                                longitude=3.5 * u.deg))
+        c = ECEF(Geodetic(latitude=45.5, longitude=3.5, height=0))
+        #z = tools.topography.geoid_undulation(c)
+        z = topography.geoid_undulation(c)
         self.assertEqual(z.size, 1)
-        self.assertEqual(z.unit, u.m)
+        #self.assertEqual(z.unit, u.m)
 
 
     def test_custom_topography(self):
         # Fetch a test tile
-        dirname, basename = Path('tests/topography'), 'N39E090.SRTMGL1.hgt'
+        #dirname, basename = Path('tests/topography'), 'N39E090.SRTMGL1.hgt'
+        dirname, basename = Path('../topography'), 'N39E090.SRTMGL1.hgt'
         path = dirname / basename
         if not path.exists():
             dirname.mkdir(exist_ok=True)
@@ -41,11 +46,12 @@ class TopographyTest(TestCase):
 
         # Test the topography getter
         topo = Topography(dirname)
-        c = ECEF(GeodeticRepresentation(latitude=39.5 * u.deg,
-                                        longitude=90.5 * u.deg))
+        #c = ECEF(GeodeticRepresentation(latitude=39.5 * u.deg,
+        #                                longitude=90.5 * u.deg))
+        c = ECEF(Geodetic(latitude=39.5, longitude=90.5, height=0))
         z = topo.elevation(c)
         self.assertEqual(z.size, 1)
-        self.assertEqual(z.unit, u.m)
+        #self.assertEqual(z.unit, u.m)
         self.assertFalse(numpy.isnan(z))
 
 
@@ -60,22 +66,28 @@ class TopographyTest(TestCase):
         self.assertFalse([p for p in topography.cachedir().glob('**/*')])
 
         # Fetch data
-        c = ECEF(GeodeticRepresentation(latitude=39.5 * u.deg,
-                                        longitude=90.5 * u.deg))
+        #c = ECEF(GeodeticRepresentation(latitude=39.5 * u.deg,
+        #                                longitude=90.5 * u.deg))
+        c = ECEF(Geodetic(latitude=39.5, longitude=90.5, height=0))
         topography.update_data(c)
         self.assertTrue(
             (topography.cachedir() / 'N39E090.SRTMGL1.hgt').exists())
 
-        c = ECEF(GeodeticRepresentation(
-            latitude=u.Quantity((39.5, 40.5)) * u.deg,
-            longitude=u.Quantity((90.5, 90.5)) * u.deg))
+        #c = ECEF(GeodeticRepresentation(
+        #    latitude=u.Quantity((39.5, 40.5)) * u.deg,
+        #    longitude=u.Quantity((90.5, 90.5)) * u.deg))
+        c = ECEF(Geodetic(latitude =numpy.array([39.5, 40.5]), 
+                          longitude=numpy.array([90.5, 90.5]), 
+                          height   =numpy.array([0, 0])))
         topography.update_data(c)
         self.assertTrue(
             (topography.cachedir() / 'N40E090.SRTMGL1.hgt').exists())
 
-        c = ECEF(GeodeticRepresentation(latitude=40 * u.deg,
-                                        longitude=90.5 * u.deg))
-        topography.update_data(c, radius=100 * u.km)
+        #c = ECEF(GeodeticRepresentation(latitude=40 * u.deg,
+        #                                longitude=90.5 * u.deg))
+        c = ECEF(Geodetic(latitude=40, longitude=90.5, height=0))
+        #topography.update_data(c, radius=100 * u.km)
+        topography.update_data(c, radius=100e3) #RK radius in meters.
         self.assertTrue(
             (topography.cachedir() / 'N39E089.SRTMGL1.hgt').exists())
         self.assertTrue(
@@ -88,72 +100,92 @@ class TopographyTest(TestCase):
 
     def test_topography_elevation(self):
         # Fetch a test tile
-        geo = GeodeticRepresentation(latitude=39.5 * u.deg,
-                                     longitude=90.5 * u.deg)
+        #geo = GeodeticRepresentation(latitude=39.5 * u.deg,
+        #                             longitude=90.5 * u.deg)
+        geo = Geodetic(latitude=39.5, longitude=90.5, height=0)
         c = ECEF(geo)
         topography.update_data(c)
 
         # Test the global topography getter
-        z0 = topography.elevation(c)
+        #z0 = topography.elevation(c)
+        z0 = topography.elevation(geo)
         self.assertEqual(z0.size, 1)
-        self.assertEqual(z0.unit, u.m)
+        #   self.assertEqual(z0.unit, u.m)
         self.assertFalse(numpy.isnan(z0))
 
-        z1 = topography.elevation(c, reference=topography.Reference.GEOID)
-        z1 += topography.geoid_undulation(c)
+        z1 = topography.elevation(geo, reference='ELLIPSOID')
+        z1 -= topography.geoid_undulation(geo)
         self.assertEqual(z1.size, 1)
-        self.assertEqual(z1.unit, u.m)
+        #self.assertEqual(z1.unit, u.m)
         self.assertFalse(numpy.isnan(z1))
 
         self.assertQuantity(z0, z1)
 
         o = numpy.ones(10)
-        cv = ECEF(GeodeticRepresentation(latitude=geo.latitude * o,
-                                         longitude=geo.longitude * o))
+        #cv = ECEF(GeodeticRepresentation(latitude=geo.latitude * o,
+        #                                 longitude=geo.longitude * o))
+        cv = ECEF(Geodetic(latitude=geo.latitude  *o,
+                           longitude=geo.longitude*o,
+                           height   = geo.height  *o))
         z2 = topography.elevation(cv)
         self.assertEqual(z2.size, o.size)
-        self.assertEqual(z2.unit, u.m)
+        #self.assertEqual(z2.unit, u.m)
         for i in range(o.size):
             self.assertQuantity(z2[i], z0)
 
         # Test the local topography getter
-        cl = LTP(0 << u.m, 0 << u.m, 0 << u.m, location=c)
-        z3 = topography.elevation(cl)
+        #cl = LTP(0 << u.m, 0 << u.m, 0 << u.m, location=c)
+        cl = LTP(x=0, y=0, z=0,location=c, orientation='NWU')
+        z3 = topography.elevation(cl, 'LOCAL')
         self.assertEqual(z3.size, 1)
-        self.assertEqual(z3.unit, u.m)
+        #self.assertEqual(z3.unit, u.m)
         self.assertQuantity(z3, z1, 7)
 
 
     def test_topography_distance(self):
         # Fetch a test tile
-        geo = GeodeticRepresentation(latitude=39.5 * u.deg,
-                                     longitude=90.5 * u.deg)
-        c = ECEF(geo)
-        topography.update_data(c)
+        #geo = GeodeticRepresentation(latitude=39.5 * u.deg,
+        #                             longitude=90.5 * u.deg)
+        #geo = Geodetic(latitude=39.5, longitude=90.5, height=0.)
+        geo = Geodetic(latitude=41.27, longitude=96.53, height=0)
+        #c = ECEF(geo)
+        topography.update_data(geo)
 
         # Test the distance getter
-        z = topography.elevation(c) + topography.geoid_undulation(c)
-        c = ECEF(GeodeticRepresentation(latitude=geo.latitude,
-                                        longitude=geo.longitude,
-                                        height=z - 1 * u.m))
-        v = LTP(0 * u.deg, 45 * u.deg, representation_type='horizontal',
-                location = c)
-        d = topography.distance(c, v, 10 * u.m)
-        self.assertEqual(d.size, 1)
-        self.assertEqual(d.unit, u.m)
-        self.assertFalse(numpy.isnan(d))
-        self.assertTrue(d < 0)
+        z = topography.elevation(geo) + topography.geoid_undulation(geo)
+        #z = topography.elevation(c) + topography.geoid_undulation(c)
+        #c = ECEF(GeodeticRepresentation(latitude=geo.latitude,
+        #                                longitude=geo.longitude,
+        #                                height=z - 1 * u.m))
+        
+        v0 = GRANDCS(x=100, y=10, z=-0.1, location=geo)
+        x0 = GRANDCS(x=-8000,y=12000,z=2200, location=geo) # Initial point along traj (in LTP frame)
+        v  = numpy.matmul(x0.basis.T, v0)  # GRANDCS --> ECEF. input direction must be in ECEF. ToDo: Fix this.
 
-        d = topography.distance(c, v, 50 * u.cm)
+        #v = LTP(0 * u.deg, 45 * u.deg, representation_type='horizontal',
+        #        location = c)
+
+        #d = topography.distance(c, v, 10 * u.m)
+        d = topography.distance(x0, v) #(pos, dir, max_dist). max_dist is optional
+
+        self.assertEqual(d.size, 1)
+        #self.assertEqual(d.unit, u.m)
+        self.assertFalse(numpy.isnan(d))
+        self.assertTrue(d > 0)
+
+        #d = topography.distance(c, v, 50 * u.cm)
+        d = topography.distance(x0, v, 50)
         self.assertTrue(numpy.isnan(d))
 
         o = numpy.ones(10)
-        c = ECEF(GeodeticRepresentation(latitude=geo.latitude * o,
-                                        longitude=geo.longitude * o,
-                                        height=(z - 1 * u.m) * o))
-        d = topography.distance(c, v, 10 * u.m)
+        #c = ECEF(GeodeticRepresentation(latitude=geo.latitude * o,
+        #                                longitude=geo.longitude * o,
+        #                                height=(z - 1 * u.m) * o))
+        c = Geodetic(latitude=41.27*o, longitude=96.53*o, height=(z-1)*o)
+        #d = topography.distance(c, v, 10 * u.m)
+        d = topography.distance(c, v*o)
         self.assertEqual(d.size, o.size)
-        self.assertEqual(d.unit, u.m)
+        #self.assertEqual(d.unit, u.m)
         for i in range(o.size):
             self.assertTrue(d[i] < 0)
 
