e3af5a85d4c7a7816be0e6cac522da2dc6350ee7
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-11-22 13:00:28 +0100

    used grand_quality_analysis in CI

--- quality/grand_quality_analysis.bash
+++ quality/grand_quality_analysis.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd $GRAND_ROOT
-pylint grand --rcfile quality/pylint.conf -r y --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee quality/report_pylint.txt
+#pylint grand --rcfile quality/pylint.conf -r y --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee quality/report_pylint.txt
+pylint grand --rcfile quality/pylint.conf --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --output=quality/report_pylint.txt -r y
+status=$?
+#pylint grand --rcfile quality/pylint.conf -r y
+cat quality/report_pylint.txt
+exit $status
