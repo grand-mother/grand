e722636982665d755d8d093e7459e1fbc9ef7dbf
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-02 14:43:18 +0100

    add option verbosity

--- quality/grand_quality_test_cov.bash
+++ quality/grand_quality_test_cov.bash
@@ -4,7 +4,7 @@ cd $GRAND_ROOT
 coverage erase
 rm -f quality/report_coverage*
 rm -rf quality/html_cov*
-coverage run --source=grand -m pytest tests 
+coverage run --source=grand -m pytest tests -v
 coverage xml -i -o quality/report_coverage.xml
 coverage html -i -d quality/html_coverage
 coverage report
