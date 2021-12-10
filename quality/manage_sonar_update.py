e1db61cd18bca955294e4f3dac5c170b1b2a296b
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-10 14:30:43 +0100

    correct test ci

--- quality/manage_sonar_update.py
+++ quality/manage_sonar_update.py
@@ -11,8 +11,8 @@ import sys
 print(os.environ["GITHUB_REF_NAME"])
 print(os.environ["USER_GIT"])
 
-SHA = os.environ["GITHUB_SHA"]
+SHA = os.environ["GITHUB_SHA"][:8]
 GITHUB_REF_NAME = os.environ["GITHUB_REF_NAME"]
-USER_GIT = os.environ["USER_GIT"][:8]
+USER_GIT = os.environ["USER_GIT"]
 
-print(USER_GIT)
+print(SHA)
