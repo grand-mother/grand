0b12ee3a79feb537e3ceedbed41d79f92d00ab3f
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-10 17:06:15 +0100

    Dev update sonar ci (#17)
    * test sonar update by ci
    
    * test update sonarqube
    
    * remove blank line
    
    * touch code to launch CI ?
    
    * restore file
    
    * correct yaml syntax
    
    * correct yaml syntax
    
    * test update SQ
    
    * test before update :-)
    
    * update version sonar-scanner and restore test CI
    
    * test github var
    
    * test var github
    
    * test var ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * add script
    
    * test ci
    
    * correct test ci
    
    * test ci
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * correction env name
    
    * move USER_GIT
    
    * add bash if block ?
    
    * correction pars
    
    * debug ci
    
    * test ci
    
    * test ci
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * with success
    
    * typo
    
    * test failed
    
    * add sys
    
    * simul failed
    
    * test ci
    
    * not with github ci
    
    * typo
    
    * add return value as parameters !!
    
    * pb path ?
    
    * with user
    
    * test with user not autorized
    
    * correct for not interruption of CI
    
    * dont't used exit to update sonarqube => kill CI !!!
    
    * finished update soanrqube update

---
+++ quality/manage_sonar_update.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+'''
+Created on 22 nov. 2021
+
+@author: jcolley
+'''
+
+import os
+import sys
+
+USER_SONAR = ['luckyjim', 'rameshkoirala', 'niess', 'grand-oma', "lwpiotr"]
+
+#==================== ENV
+SHA = "sha:" + os.environ["GITHUB_SHA"][:8]
+BRANCH = os.environ["GITHUB_REF_NAME"]
+USER_GIT = os.environ["USER_GIT"]
+LOGIN = "0c9f679e12df438a312b424ae52f5959cd0b5e4a"
+
+
+#==================== FUNCTION
+def create_sonar_properties(name="", key="", descr="", login="", n_file='sonar.properties'):
+    properties = f'sonar.projectName={name}'
+    properties += f'\nsonar.projectKey={key}'
+    properties += f'\nsonar.projectDescription={descr}'
+    properties += f'\nsonar.login={login}'
+    properties += '''
+sonar.host.url=https://sonarqube.grand.in2p3.fr
+
+sonar.sources=grand
+sonar.sourceEncoding=UTF-8
+sonar.python.coverage.reportPaths=quality/report_coverage.xml
+sonar.python.pylint.reportPaths=quality/report_pylint.txt
+'''
+    with open(n_file, 'w') as f_job:
+        f_job.write(properties)
+        print(n_file)
+    print(properties)
+    
+    
+#==================== MAIN
+if BRANCH in ['master', 'dev']:
+    name = BRANCH
+    # name = 'grand'   
+    create_sonar_properties(name, name, SHA, LOGIN)    
+elif USER_GIT in USER_SONAR:
+    name = f'user_{USER_GIT}'
+    create_sonar_properties(name, name, BRANCH, LOGIN)
+else:
+    print("No update SonarQube necessary")
+sys.exit(0)
+    
