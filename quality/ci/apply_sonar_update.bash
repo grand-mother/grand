bf5efc19d15b427998a0eff5df6ceaf93f38d647
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-13 12:07:36 +0100

    apply pylint with and always exit 0 for sonar

--- quality/ci/apply_sonar_update.bash
+++ quality/ci/apply_sonar_update.bash
@@ -9,7 +9,7 @@ if [ -f "$FILE" ]; then
 	PATH=$PATH:$GRAND_ROOT/sonar/sonar-scanner-4.6.2.2472-linux/bin
 	cd $GRAND_ROOT
 	grand_quality_test_cov.bash
-	grand_quality_analysis.bash
+	pylint grand --rcfile quality/pylint.conf --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --output=quality/report_pylint.txt -r y --exit-zero
 	sonar-scanner -Dproject.settings=$FILE
 fi 
 # always ok for github CI
