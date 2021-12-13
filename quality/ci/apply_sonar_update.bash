eab3a1d3e68954b4c8d933d9b3c0551e57c1b059
Colley JM <jcolley@lpnhe.in2p3.fr>
2021-12-13 12:14:16 +0100

    add pylint report

--- quality/ci/apply_sonar_update.bash
+++ quality/ci/apply_sonar_update.bash
@@ -10,6 +10,7 @@ if [ -f "$FILE" ]; then
 	cd $GRAND_ROOT
 	grand_quality_test_cov.bash
 	pylint grand --rcfile quality/pylint.conf --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" --output=quality/report_pylint.txt -r y --exit-zero
+	cat quality/report_pylint.txt
 	sonar-scanner -Dproject.settings=$FILE
 fi 
 # always ok for github CI
