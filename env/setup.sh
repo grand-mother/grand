7935fec2716a40b5b942e6574355d4436b979661
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-27 16:54:58 +0100

    test CI simplify setup.sh

--- env/setup.sh
+++ env/setup.sh
@@ -1,56 +1,12 @@
 #! /bin/bash
 
-main () {
-    local OPTIND=1 option quiet
-    while getopts "q" option
-    do
-    case "${option}"
-    in
-        q) quiet=true;;
-    esac
-    done
+local prefix="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+# add to env variable define root pf package
+export GRAND_ROOT=$prefix
+export PATH=$PATH:$GRAND_ROOT/quality
+export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
 
-    local prefix="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-    # add to env variable define root pf package
-    export GRAND_ROOT=$prefix
-    local tag="$(basename ${prefix})"
-    logmsg () {
-        [[ -z "${quiet}" ]] && echo "[${tag}] $@"
-    }
-
-
-    logmsg "--Setting the environment"
-
-    # Expand the PATH
-    expand_path () {
-        local path="${prefix}/${1}"
-        if [[ ! "$PATH" =~ "${path}" ]]; then
-            logmsg "  Adding \$PREFIX/${1} to PATH"
-            export PATH="${path}:${PATH}"
-        fi
-    }
-
-    # add in PATH quality for script
-    expand_path "quality"
-
-    expand_pythonpath () {
-        local path="${prefix}/${1}"
-        if [[ ! "$PYTHONPATH" =~ "${path}" ]]; then
-            logmsg "  Adding \$PREFIX/${1} to PYTHONPATH"
-            export PYTHONPATH="${path}:${PYTHONPATH}"
-        fi
-    }
-
-    expand_pythonpath ""
-
-    # Build the C extensions
-	cd $GRAND_ROOT/src
-	./install_ext_lib.bash
-	cd -
-	
-    logmsg "--Environment set"
-}
-
-
-main "$@"
-unset -f expand_path logmsg main sanitize_shebangs
+cd $GRAND_ROOT/src
+./install_ext_lib.bash
+cd -
+	 
