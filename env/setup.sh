20fdab39519f94e8ca211205bf8a8bdc2d6f111e
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-27 17:08:46 +0100

    test CI

--- env/setup.sh
+++ env/setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-export GRAND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+#export GRAND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+export GRAND_ROOT=$1
 echo $GRAND_ROOT
 export PATH=$PATH:$GRAND_ROOT/quality
 export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
@@ -8,4 +9,3 @@ export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
 cd $GRAND_ROOT/src
 ./install_ext_lib.bash
 cd -
-	 
