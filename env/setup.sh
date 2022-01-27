6372b175a3078f448253384948ed6b9b451cbca7
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-27 17:15:56 +0100

    test CI

--- env/setup.sh
+++ env/setup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-
+echo "param"
+echo $1
 #export GRAND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 export GRAND_ROOT=$1
+echo "GRAND_ROOT"
 echo $GRAND_ROOT
 export PATH=$PATH:$GRAND_ROOT/quality
 export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
