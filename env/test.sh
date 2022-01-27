036cdeffbf630d91e90847c69d5c93ad7187a21f
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-27 17:32:56 +0100

    copy on test.sh

--- env/test.sh
+++ env/test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo $1
-echo "FIN"
+#echo $1
+#echo "FIN"
+export GRAND_ROOT=$1
+echo "GRAND_ROOT"
+echo $GRAND_ROOT
+export PATH=$PATH:$GRAND_ROOT/quality
+export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
+cd $GRAND_ROOT/src
+./install_ext_lib.bash
+cd -
