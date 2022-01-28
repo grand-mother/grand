6a632726a8bb88cdc6be1396ba47fdc25bd13bcd
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-28 10:21:17 +0100

    test with new docker image

--- env/docker/docker_build_push.sh
+++ env/docker/docker_build_push.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-tag="0.2"
+tag=0.4
 
 docker build -f Dockerfile_grand_env_dev . --tag=grand_env:$tag
 
-docker tag grand_env:$tag jcolley/grand:$tag
-docker push jcolley/grand:$tag
+#docker tag grand_env:$tag jcolley/grand:$tag
+#docker push jcolley/grand:$tag
