baa9d4d2b56a92276054b4701a1ededcfd65a279
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-31 11:13:16 +0100

    docker images with different layers

--- env/docker/docker_build_push.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-tag=0.4
-
-docker build -f Dockerfile_grand_env_dev . --tag=grand_env:$tag
-
-#docker tag grand_env:$tag jcolley/grand:$tag
-#docker push jcolley/grand:$tag
--- env/setup.sh
+++ env/setup.sh
@@ -2,12 +2,17 @@
 
 export GRAND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 echo "Set var GRAND_ROOT="$GRAND_ROOT
+echo "=============================="
 export PATH=$PATH:$GRAND_ROOT/quality
 echo "update PATH with grand quality"
+echo "=============================="
+
 export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
 echo "update PYTHONPATH with grand source"
+echo "=============================="
 
 echo "Install external lib gull and turtle"
+echo "===================================="
 cd $GRAND_ROOT/src
 ./install_ext_lib.bash
 cd $GRAND_ROOT
