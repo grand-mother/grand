6343a30f28e847d5b93b6f7351b4e9d2f0c5e1c8
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-01-31 11:52:55 +0100

    split setup.sh and update

---
+++ env/_setup_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Install external lib gull and turtle"
+echo "===================================="
+cd $GRAND_ROOT/src
+./install_ext_lib.bash
+cd $GRAND_ROOT
--- env/setup.sh
+++ env/setup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-export GRAND_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-echo "Set var GRAND_ROOT="$GRAND_ROOT
-echo "=============================="
-export PATH=$PATH:$GRAND_ROOT/quality
-echo "update PATH with grand quality"
-echo "=============================="
-
-export PYTHONPATH=$PYTHONPATH:$GRAND_ROOT
-echo "update PYTHONPATH with grand source"
-echo "=============================="
-
-echo "Install external lib gull and turtle"
-echo "===================================="
-cd $GRAND_ROOT/src
-./install_ext_lib.bash
-cd $GRAND_ROOT
+. _setup_env.sh
+. _setup_lib.sh
