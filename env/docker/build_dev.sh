668c5bdcfc21f49254ef730d1a4da10d335627c1
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-02-03 12:14:55 +0100

    add grandlib development image

---
+++ env/docker/build_dev.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+
+tag=latest
+
+docker rmi grandlib_dev:latest
+
+cp ../../quality/requirements.txt requirements_qual.txt
+cp ../../docs/apidoc-only/doxygen-rtd/requirements.txt requirements_docs.txt
+
+
+docker build -f Dockerfile_dev2 . --tag=grandlib_dev:$tag
+
+
+#docker tag grand_env:$tag jcolley/grandlib_ci:$tag
+#docker push jcolley/grandlib_ci:$tag
