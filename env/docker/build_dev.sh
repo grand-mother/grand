fb3cb47505716c57b607fada914a3ec9cbebc600
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-02-03 13:03:49 +0100

    format

--- env/docker/build_dev.sh
+++ env/docker/build_dev.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 tag=latest
 
 docker rmi grandlib_dev:$tag
@@ -8,9 +7,4 @@ docker rmi grandlib_dev:$tag
 cp ../../quality/requirements.txt requirements_qual.txt
 cp ../../docs/apidoc-only/doxygen-rtd/requirements.txt requirements_docs.txt
 
-
 docker build -f Dockerfile_dev2 . --tag=grandlib_dev:$tag
-
-
-#docker tag grand_env:$tag jcolley/grandlib_ci:$tag
-#docker push jcolley/grandlib_ci:$tag
