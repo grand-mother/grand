15a4654c036685e11f200500e94f120892ddfc86
Lech Piotrowski <science@lwp.email>
2022-03-25 15:18:30 +0100

    Cosmetic changes

--- examples/io/DataReadingExample.py
+++ examples/io/DataReadingExample.py
@@ -12,6 +12,7 @@ if len(sys.argv)<2:
 else:
     tadccounts = ADCEventTree(sys.argv[1])
 
+#tadccounts.get_event(10002, 2201)
 tadccounts.get_event(2)
 print("ADCCounts readout: tadccounts.event_number, tadccounts.time_seconds, tadccounts.trace_0[0]")
 print(tadccounts.event_number, tadccounts.time_seconds, tadccounts.trace_0)
@@ -20,8 +21,9 @@ print(tadccounts.event_number, tadccounts.time_seconds, tadccounts.trace_0)
 tefield = EfieldEventTree("stored_data.root")
 #tvoltage = GRANDVoltageTree("stored_data.root")
 
+# tefield.get_event(10002, 2201)
 tefield.get_event(2)
 print("\nEfield readout: tefield.event_number, tefield.det_time[0], tefield.trace_x[0][0], tadccounts.evt_id")
 print("The event_number of tadccounts changed to 4 when tefield event with event_number 4 was requested")
-print(tefield.event_number, tefield.det_time[0], tefield.trace_x[0][0], tadccounts.event_number)
+print(tefield.event_number, tefield.du_seconds[0], tefield.trace_x[0][0], tadccounts.event_number)
 
