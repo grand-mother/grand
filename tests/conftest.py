d3821aaac1f0cfc5a622564e49e2eddb631b7871
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-07-01 22:11:33 +0200

    rename, clear code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,14 @@
 """configuration file for pytest
 """
-import sys
+
 import os
 import os.path as osp
-
 from urllib import request
-import pytest
 
 from grand import GRAND_DATA_PATH
-from grand.simu import TabulatedAntennaModel
-
 
 
 GRAND_ROOT = os.getenv("GRAND_ROOT")
-
 DEPOT_GITHUB = "https://github.com/grand-mother/store/releases/download/101"
 
 
@@ -21,20 +16,20 @@ def pytest_configure(config):
     """Run master_data_test main function prior to run pytests."""
     if not osp.exists(GRAND_DATA_PATH):
         os.mkdir(GRAND_DATA_PATH)
-    grand_download_big_file()
+    grand_download_huge_file()
     # add logger message
     #mlg.create_output_for_logger("debug", log_stdout=True)
 
 
-def grand_download_big_file(depot=DEPOT_GITHUB):
+def grand_download_huge_file(depot=DEPOT_GITHUB):
     name_file = "HorizonAntenna_EWarm_leff_loaded.npy"
-    download_big_file(
+    download_huge_file(
         f"{depot}/{name_file}",
         osp.join(GRAND_DATA_PATH, name_file),
     )
 
 
-def download_big_file(url_path, grand_path):
+def download_huge_file(url_path, grand_path):
     if not osp.isfile(grand_path):
         request.urlretrieve(url_path, grand_path)
         print(f"Successfully downloaded the file {grand_path}")
