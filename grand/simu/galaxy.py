ff1e6723fa66042eab2418629ed0f956e9099786
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-07-20 12:30:22 +0200

    improve code quality

--- grand/simu/galaxy.py
+++ grand/simu/galaxy.py
@@ -12,7 +12,7 @@ from grand.num.signal import complex_expansion, ifftget
 from grand import grand_add_path_data
 
 
-def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
+def galaxy_radio_signal(lst, size_out, freq_samp, freq_1, nb_ant, show_flag=False):
     """!
     This program is used as a subroutine to complete the calculation and
     expansion of galactic noise
@@ -21,8 +21,9 @@ def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
 
     @param lst：Select the galactic noise LST at the LST moment
     @param size_out (int): is the extended length
-    @param f0 (float): is the frequency resolution,
-    @param f1 (float): is the frequency point of the unilateral spectrum
+    @param freq_samp (float): is the frequency resolution,
+    #TODO: freq_1 description not clear
+    @param freq_1 (float): is the frequency point of the unilateral spectrum 
     @param show_flag (bool): print figure
 
     @return : v_complex_double, galactic_v_time
@@ -67,20 +68,22 @@ def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
     #
     f_start = 30
     f_end = 250
+    # TODO: 221 is the number of frequency ? why ? and comment to explain
+    nb_freq = 221
     v_complex_double = np.zeros((nb_ant, size_out, 3), dtype=complex)
     galactic_v_time = np.zeros((nb_ant, size_out, 3), dtype=float)
     galactic_v_m_single = np.zeros((nb_ant, int(size_out / 2) + 1, 3), dtype=float)
     galactic_v_p_single = np.zeros((nb_ant, int(size_out / 2) + 1, 3), dtype=float)
-    V_amplitude = gala_voltage[:, :, lst - 1]
-    a_nor = np.zeros((nb_ant, 221, 3), dtype=float)
-    phase = np.zeros((nb_ant, 221, 3), dtype=float)
-    v_complex = np.zeros((nb_ant, 221, 3), dtype=complex)
+    v_amplitude = gala_voltage[:, :, lst - 1]
+    a_nor = np.zeros((nb_ant, nb_freq, 3), dtype=float)
+    phase = np.zeros((nb_ant, nb_freq, 3), dtype=float)
+    v_complex = np.zeros((nb_ant, nb_freq, 3), dtype=complex)
     for l_ant in range(nb_ant):
-        for l_fq in range(221):
+        for l_fq in range(nb_freq):
             for l_axis in range(3):
-                # Generates a normal distribution with 0 as the mean and V_amplitude[l_fq, l_axis] as the standard deviation
+                # Generates a normal distribution with 0 as the mean and v_amplitude[l_fq, l_axis] as the standard deviation
                 a_nor[l_ant, l_fq, l_axis] = np.random.normal(
-                    loc=0, scale=V_amplitude[l_fq, l_axis]
+                    loc=0, scale=v_amplitude[l_fq, l_axis]
                 )
                 # phase of random Gauss noise
                 phase[l_ant, l_fq, l_axis] = 2 * np.pi * random.random()
@@ -91,7 +94,7 @@ def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
         for port in range(3):
             [_, v_complex_double[l_ant, :, port]] = complex_expansion(
                 size_out,
-                f0,
+                freq_samp,
                 f_start,
                 f_end,
                 v_complex[l_ant, :, port],
@@ -99,7 +102,7 @@ def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
         [galactic_v_time[l_ant], galactic_v_m_single[l_ant], galactic_v_p_single[l_ant]] = ifftget(
             v_complex_double[l_ant],
             size_out,
-            f1,
+            freq_1,
             2,
         )
     return v_complex_double, galactic_v_time
