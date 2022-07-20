ca556b63aca1b140d933278b36fa67218f0054bd
Colley JM <jcolley@lpnhe.in2p3.fr>
2022-07-20 02:15:25 +0200

    improve coding rules

--- grand/simu/galaxy.py
+++ grand/simu/galaxy.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from grand.num.signal import complex_expansion, ifftget
-from grand import grand_add_path_data, grand_get_path_root_pkg
+from grand import grand_add_path_data
 
 
 def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
@@ -60,44 +60,42 @@ def galaxy_radio_signal(lst, size_out, f0, f1, nb_ant, show_flag=False):
     gala_psd_dbm = np.transpose(gala_show["psd_narrow_huatu"])
     gala_power_dbm = np.transpose(gala_show["p_narrow_huatu"])
     gala_voltage = np.transpose(gala_show["v_amplitude"])
-    gala_power_mag = np.transpose(gala_show["p_narrow"])
+    #gala_power_mag = np.transpose(gala_show["p_narrow"])
     gala_freq = gala_show["freq_all"]
     if show_flag:
         plot()
     #
     f_start = 30
     f_end = 250
-    R = 50
     v_complex_double = np.zeros((nb_ant, size_out, 3), dtype=complex)
     galactic_v_time = np.zeros((nb_ant, size_out, 3), dtype=float)
     galactic_v_m_single = np.zeros((nb_ant, int(size_out / 2) + 1, 3), dtype=float)
     galactic_v_p_single = np.zeros((nb_ant, int(size_out / 2) + 1, 3), dtype=float)
-    unit_uv = 1e6
     V_amplitude = gala_voltage[:, :, lst - 1]
-    aa = np.zeros((nb_ant, 221, 3), dtype=float)
+    a_nor = np.zeros((nb_ant, 221, 3), dtype=float)
     phase = np.zeros((nb_ant, 221, 3), dtype=float)
     v_complex = np.zeros((nb_ant, 221, 3), dtype=complex)
-    for mm in range(nb_ant):
-        for ff in range(221):
-            for pp in range(3):
-                # Generates a normal distribution with 0 as the mean and V_amplitude[ff, pp] as the standard deviation
-                aa[mm, ff, pp] = np.random.normal(loc=0, scale=V_amplitude[ff, pp])
+    for l_ant in range(nb_ant):
+        for l_fq in range(221):
+            for l_axis in range(3):
+                # Generates a normal distribution with 0 as the mean and V_amplitude[l_fq, l_axis] as the standard deviation
+                a_nor[l_ant, l_fq, l_axis] = np.random.normal(loc=0, scale=V_amplitude[l_fq, l_axis])
                 # phase of random Gauss noise
-                phase[mm, ff, pp] = 2 * np.pi * random.random()
-                v_complex[mm, ff, pp] = abs(aa[mm, ff, pp] * size_out / 2)
-                v_complex[mm, ff, pp] *= np.exp(1j * phase[mm, ff, pp])
+                phase[l_ant, l_fq, l_axis] = 2 * np.pi * random.random()
+                v_complex[l_ant, l_fq, l_axis] = abs(a_nor[l_ant, l_fq, l_axis] * size_out / 2)
+                v_complex[l_ant, l_fq, l_axis] *= np.exp(1j * phase[l_ant, l_fq, l_axis])
     #
-    for kk in range(nb_ant):
+    for l_ant in range(nb_ant):
         for port in range(3):
-            [freq, v_complex_double[kk, :, port]] = complex_expansion(
+            [_, v_complex_double[l_ant, :, port]] = complex_expansion(
                 size_out,
                 f0,
                 f_start,
                 f_end,
-                v_complex[kk, :, port],
+                v_complex[l_ant, :, port],
             )
-        [galactic_v_time[kk], galactic_v_m_single[kk], galactic_v_p_single[kk]] = ifftget(
-            v_complex_double[kk],
+        [galactic_v_time[l_ant], galactic_v_m_single[l_ant], galactic_v_p_single[l_ant]] = ifftget(
+            v_complex_double[l_ant],
             size_out,
             f1,
             2,
