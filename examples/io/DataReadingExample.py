a4975c25cf7c4ca7e2844e1499021d61a583870d
Lech Piotrowski <science@lwp.email>
2022-08-01 17:21:21 +0200

    Reading an event from the event list of the tree

--- examples/io/DataReadingExample.py
+++ examples/io/DataReadingExample.py
@@ -7,19 +7,25 @@ from grand.io.root_trees import *
 if len(sys.argv)<2:
     tadccounts = ADCEventTree("stored_data.root")
 else:
+    print(f"Reading file {sys.argv[1]}")
     tadccounts = ADCEventTree(sys.argv[1])
 
-#tadccounts.get_event(10002, 2201)
-tadccounts.get_event(2)
+# Print out the list of runs,events
+tadccounts.print_list_of_events()
+# Get the list of runs,events
+list_of_events = tadccounts.get_list_of_events()
+# Read the first event from the list of events
+tadccounts.get_event(*list_of_events[0])
 print("ADCCounts readout: tadccounts.event_number, tadccounts.time_seconds, tadccounts.trace_0[0]")
 print(tadccounts.event_number, tadccounts.time_seconds, tadccounts.trace_0[0])
+print(tadccounts.trace_0[0][0])
+exit()
 
 
 tefield = EfieldEventTree("stored_data.root")
 #tvoltage = GRANDVoltageTree("stored_data.root")
 
-# tefield.get_event(10002, 2201)
-tefield.get_event(2)
+tefield.get_event(*list_of_events[2])
 print("\nEfield readout: tefield.event_number, tefield.det_time[0], tefield.trace_x[0][0], tadccounts.evt_id")
 print("The event_number of tadccounts changed to 4 when tefield event with event_number 4 was requested")
 print(tefield.event_number, tefield.du_seconds[0], tefield.trace_x[0][0], tadccounts.event_number)
