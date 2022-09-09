0895dece64a9beb705f7fa917393696b450e3e13
luckyjim <jcolley@lpnhe.in2p3.fr>
2022-09-09 14:13:36 +0000

    trace container avec numpy

--- grand/store/trace.py
+++ grand/store/trace.py
@@ -1,12 +1,11 @@
 '''
-simple event table 
+prototype to store trace from io.root with numpy array
 '''
+
 import grand.io.root.event.efield as ioree
 import numpy as np
 
-
-class TraceTableDC1(object):
-    '''
+'''
     debut gerb : 
     sE=ior.EfieldEventTree('Coarse1.root')
     sE.get_list_of_events()
@@ -18,6 +17,12 @@ class TraceTableDC1(object):
     sE.trace_x efield sur x
     '''
 
+    
+class TraceTableDC1(object):
+    '''
+    use one structured numpy array ~ astropy Table
+    '''
+    
     def __init__(self):
         '''
         Constructor
@@ -61,4 +66,58 @@ class TraceTableDC1(object):
         self.t_trace['y'] = self.ttree_evt.trace_y
         self.t_trace['z'] = self.ttree_evt.trace_z
         return True
+
+
+class TraceArrayDC1(object):
+    '''
+    use sevral numpy array to store trace and id du , time
+    '''    
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.unit_trace = ""
+        # table of trace of event
+        self.a_du = None
+        self.a_tps = None
+        self.a3_trace = None
+        self.nb_events = -1
+        # number detector in event
+        self.nb_det = -1 
+        self.idx_event = -1
+
+    def read_efield_event(self, f_name):
+        sE = ioree.EfieldEventTree(f_name)
+        self.ttree_evt = sE
+        # hard coding event number
+        self.l_events = sE.get_list_of_events()
+        self.nb_events = len(self.l_events)
+        if len(self.l_events) == 0:
+            raise
+        self.idx_event = -1
+        return self.next_event()      
     
+    def next_event(self):
+        self.idx_event += 1
+        if self.idx_event >= self.nb_events:
+            return False
+        idx_event = self.l_events[self.idx_event]
+        self.ttree_evt.get_event(idx_event[0], idx_event[1])
+        self.nb_det = self.ttree_evt.du_count
+        self.trace_size = len(self.ttree_evt.trace_x[0])
+        self.a3_trace = np.empty((self.nb_det, 3, self.trace_size), dtype=np.float32)
+        self.t_du = np.array(self.ttree_evt.du_id, dtype=np.int16)
+        self.t_tps = np.array(self.ttree_evt.du_nanoseconds, dtype=np.float32)
+        self.a3_trace[:, 0,:] = np.array(self.ttree_evt.trace_x, dtype=np.float32)
+        self.a3_trace[:, 1,:] = np.array(self.ttree_evt.trace_y, dtype=np.float32)
+        self.a3_trace[:, 2,:] = np.array(self.ttree_evt.trace_z, dtype=np.float32)
+        return True
+    
+    def find_max_trace(self):
+        '''
+        find absolute maximal vale in trace for each detector
+        :param self:
+        '''
+        return np.max(np.abs(self.a3_trace), axis=(1, 2))
+
