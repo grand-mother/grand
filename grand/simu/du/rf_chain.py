6e6979139a4349797c846b34f14a8ee34eace5c9
luckyjim <jcolley@lpnhe.in2p3.fr>
2022-10-24 08:47:54 +0000

    external freq definition for output (remove expan() function), use derivation for GP300 files model

--- grand/simu/du/rf_chain.py
+++ grand/simu/du/rf_chain.py
@@ -5,6 +5,8 @@ Reference document :
   * "RF Chain simulation for GP300" by Xidian University, 
     Pengfei Zhang and 
     Pengxiong Ma, Chao Zhang, Rongjuan Wand, Xin Xu
+    
+  * Article 
 
 Reference code: 
   * https://github.com/JuliusErv1ng/XDU-RF-chain-simulation/blob/main/XDU%20RF%20chain%20code.py
@@ -19,70 +21,48 @@ from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 
-from grand.num.signal import halfcplx_fullcplx_padding
 from grand import grand_add_path_data_model
 
 logger = getLogger(__name__)
 
 
-def plot_csv_file(
-    f_name,
-    col_x,
-    col_y,
-    x_label="MHz",
-    y_label="TBD",
-):  # pragma: no cover
-    plt.figure()
-    plt.title(f"{f_name}, x:{col_x}, y:{col_y}")
-    data = np.loadtxt(f_name)
-    plt.plot(data[:, col_x], data[:, col_y])
-    plt.ylabel(x_label)
-    plt.xlabel(y_label)
-    plt.grid()
-
-
-def plot_csv_file_abs(
-    f_name,
-    col_x,
-    col_y1,
-    col_y2,
-    x_label="MHz",
-    y_label="TBD",
-):  # pragma: no cover
-    plt.figure()
-    plt.title(f"{f_name}, x:{col_x}, ")
-    data = np.loadtxt(f_name)
-    plt.plot(data[:, col_x], np.abs(data[:, col_y1] + 1j * data[:, col_y2]))
-    plt.ylabel(x_label)
-    plt.xlabel(y_label)
-    plt.grid()
+def func_interpol(a_x, a_y):
+    """!
+    function of interpolation, set to zero outside interval definition a_x
+    """
+    assert a_x.shape[0] > 0
+    return interpolate.interp1d(a_x, a_y, "cubic", bounds_error=False, fill_value=(0.0, 0.0))
 
 
 class GenericProcessingDU:
+    """ """
+
     def __init__(self):
         """ """
-        self.f_step = 1.0
-        self.f_start = 30.0
-        self.f_end = 250.0
-        self.f_nb = 221
+        self.freqs_out = np.zeros(0)
+        self.size_out = 0
+
+    def _set_name_data_file(self, axis):
+        """!
+
+        @param axis:
+        """
+        # fix a file version for processing by heritage
+        raise
 
     ### SETTER
 
-    def set_frequency_band(self, f_start, f_end, nb_band):
+    def set_out_freq_mhz(self, a_freq):
         """!
-        @param f_start (float): [MHz]
-        @param f_end (float): [MHz]
-        @param nb_band (integer):
+        typically the return of scipy.fft.rfftfreq/1e6
         """
-        assert f_end > f_start > 0
-        assert nb_band > 0
-        self.f_start = f_start
-        self.f_end = f_end
-        self.f_step = (f_end - f_start) / (1.0 * nb_band)
-        self.f_nb = int(nb_band + 1)
+        assert isinstance(a_freq, np.ndarray)
+        assert a_freq[0] == 0
+        self.freqs_out = a_freq
+        self.size_out = a_freq.shape[0]
 
 
-class StandingWaveRatioGP300(GenericProcessingDU):
+class StandingWaveRatio(GenericProcessingDU):
     """!
     @authors PengFei Zhang and Xidian group, GRANDLIB adaption Colley jm
 
@@ -94,22 +74,14 @@ class StandingWaveRatioGP300(GenericProcessingDU):
 
     def __init__(self):
         super().__init__()
-        self.s11 = np.zeros((self.f_nb, 3), dtype=np.complex64)
-
-    def _set_name_data_file(self, axis):
-        """!
-
-        @param axis:
-        """
-        lna_address = os.path.join("detector", "antennaVSWR", f"{axis+1}.s1p")
-        return grand_add_path_data_model(lna_address)
+        self.s11 = np.zeros((0, 3), dtype=np.complex64)
 
     def compute_s11(self, unit=1):
         """!
 
         @param unit:
         """
-        s11 = np.zeros((self.f_nb, 3), dtype=np.complex64)
+        s11 = np.zeros((self.size_out, 3), dtype=np.complex64)
         for axis in range(3):
             filename = self._set_name_data_file(axis)
             freq = np.loadtxt(filename, usecols=0) / 1e6  # HZ to MHz
@@ -127,40 +99,58 @@ class StandingWaveRatioGP300(GenericProcessingDU):
                 db_s11 = np.zeros((3, len(freq)))
             db_s11[axis] = db
             #
-            freqnew = np.linspace(self.f_start, self.f_end, self.f_nb)
-            f_re = interpolate.interp1d(freq, re, kind="cubic")
-            renew = f_re(freqnew)
-            f_im = interpolate.interp1d(freq, im, kind="cubic")
-            imnew = f_im(freqnew)
+            f_re = func_interpol(freq, re)
+            renew = f_re(self.freqs_out)
+            f_im = func_interpol(freq, im)
+            imnew = f_im(self.freqs_out)
             s11[:, axis] = renew + 1j * imnew
-        self.f_s11 = freqnew
         self.s11 = s11
         self.f_name = filename
-        self.db_s11 = db_s11
-        self.f_db_s11 = freq
+        self._db_s11 = db_s11
+        self._f_db_s11 = freq
 
     def plot_vswr(self):  # pragma: no cover
         fig, (ax1, ax2) = plt.subplots(1, 2)
         fig.suptitle(f"VSWR, s11 parameter")
         ax1.set_title("db")
-        ax1.plot(self.f_db_s11, self.db_s11[0], label="0")
-        ax1.plot(self.f_db_s11, self.db_s11[1], label="1")
-        ax1.plot(self.f_db_s11, self.db_s11[2], label="2")
+        ax1.plot(self._f_db_s11, self._db_s11[0], label="0")
+        ax1.plot(self._f_db_s11, self._db_s11[1], label="1")
+        ax1.plot(self._f_db_s11, self._db_s11[2], label="2")
         ax1.set_ylabel(f"db")
         ax1.set_xlabel(f"[MHz]")
         ax1.grid()
         ax1.legend()
         ax2.set_title("abs(s_11)")
-        ax2.plot(self.f_s11, np.abs(self.s11[:, 0]), label="0")
-        ax2.plot(self.f_s11, np.abs(self.s11[:, 1]), label="1")
-        ax2.plot(self.f_s11, np.abs(self.s11[:, 2]), label="2")
+        ax2.plot(self.freqs_out, np.abs(self.s11[:, 0]), label="0")
+        ax2.plot(self.freqs_out, np.abs(self.s11[:, 1]), label="1")
+        ax2.plot(self.freqs_out, np.abs(self.s11[:, 2]), label="2")
         ax2.set_ylabel(f"?")
         ax2.set_xlabel(f"[MHz]")
         ax2.grid()
         ax2.legend()
 
 
-class LowNoiseAmplificatorGP300(GenericProcessingDU):
+class StandingWaveRatioGP300(StandingWaveRatio):
+    """!
+    @authors PengFei Zhang and Xidian group, GRANDLIB adaptation Colley jm
+
+    Class goals:
+      * define data model used for GP300
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def _set_name_data_file(self, axis):
+        """!
+
+        @param axis:
+        """
+        lna_address = os.path.join("detector", "antennaVSWR", f"{axis+1}.s1p")
+        return grand_add_path_data_model(lna_address)
+
+
+class LowNoiseAmplificator(GenericProcessingDU):
     """!
     @authors PengFei Zhang and Xidian group, GRANDLIB adaption Colley jm
 
@@ -170,13 +160,12 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
       * pre_compute interpolation
     """
 
-    def __init__(self, size_sig):
+    def __init__(self):
         """
 
         @param size_sig: size of the trace after
         """
         super().__init__()
-        self.size_sig = size_sig
         self.data_lna = []
         for axis in range(3):
             lna = np.loadtxt(self._set_name_data_file(axis))
@@ -184,17 +173,8 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
             lna[0] /= 1e6
             self.data_lna.append(lna)
         self.f_lna = lna[:, 0]
-        self._pre_compute()
 
     ### INTERNAL
-
-    def _set_name_data_file(self, axis):
-        """!
-
-        @param axis:
-        """
-        lna_address = os.path.join("detector", "LNASparameter", f"{axis+1}.s2p")
-        return grand_add_path_data_model(lna_address)
 
     def _pre_compute(self, unit=1):
         """!
@@ -202,13 +182,13 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
 
         @param unit: select LNA type stockage 0: [re, im],  1: [amp, arg]
         """
-        lna_gama = np.zeros((self.size_sig, 3), dtype=np.complex64)
-        lna_s21 = np.zeros((self.size_sig, 3), dtype=np.complex64)
+        lna_gama = np.zeros((self.size_out, 3), dtype=np.complex64)
+        lna_s21 = np.zeros((self.size_out, 3), dtype=np.complex64)
         dbs21_a = np.zeros((3, self.f_lna.size))
         logger.debug(f"{dbs21_a.shape}")
         logger.debug(f"{self.data_lna[0].shape}")
         for axis in range(3):
-            freq = self.data_lna[axis][:, 0]
+            freq = self.data_lna[axis][:, 0] / 1e6  # HZ to MHz
             if unit == 0:
                 res11 = self.data_lna[axis][:, 1]
                 ims11 = self.data_lna[axis][:, 2]
@@ -229,82 +209,68 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
                 res21 = mags21 * np.cos(np.deg2rad(degs21))
                 ims21 = mags21 * np.sin(np.deg2rad(degs21))
             dbs21_a[axis] = dbs21
-            freqnew = np.linspace(self.f_start, self.f_end, self.f_nb)
-            f_res11 = interpolate.interp1d(freq, res11, kind="cubic")
-            res11new = f_res11(freqnew)
-            f_ims11 = interpolate.interp1d(freq, ims11, kind="cubic")
-            ims11new = f_ims11(freqnew)
-            s11 = res11new + 1j * ims11new
-            [f_expan, lna_gama[:, axis]] = halfcplx_fullcplx_padding(
-                self.size_sig,
-                self.f_step,
-                self.f_start,
-                self.f_end,
-                s11,
-            )
-            f_res21 = interpolate.interp1d(freq, res21, kind="cubic")
-            res21new = f_res21(freqnew)
-            f_ims21 = interpolate.interp1d(freq, ims21, kind="cubic")
-            ims21new = f_ims21(freqnew)
-            s21 = res21new + 1j * ims21new
-            [f_expan, lna_s21[:, axis]] = halfcplx_fullcplx_padding(
-                self.size_sig,
-                self.f_step,
-                self.f_start,
-                self.f_end,
-                s21,
-            )
+            #
+            f_res11 = func_interpol(freq, res11)
+            res11new = f_res11(self.freqs_out)
+            f_ims11 = func_interpol(freq, ims11)
+            ims11new = f_ims11(self.freqs_out)
+            lna_gama[:, axis] = res11new + 1j * ims11new
+            #
+            f_res21 = func_interpol(freq, res21)
+            res21new = f_res21(self.freqs_out)
+            f_ims21 = func_interpol(freq, ims21)
+            ims21new = f_ims21(self.freqs_out)
+            lna_s21[:, axis] = res21new + 1j * ims21new
         # Add attribut
         self.lna_gama = lna_gama
         self.lna_s21 = lna_s21
-        self.dbs21_a = dbs21_a
-        self.f_expan = f_expan
+        self._dbs21_a = dbs21_a
 
     ### OPERATION
 
-    def update_with_s11(self, s11_short):
+    def update_with_s11(self, antenna_gama):
         """!
         @note:
           lll
 
-        @param s11_short (N,3):
+        @param antenna_gama (N,3):
         """
-        assert s11_short.shape[1] == 3
+        assert antenna_gama.shape[0] > 0
+        assert antenna_gama.shape[1] == 3
+        self._pre_compute()
         z_0 = 50
-        antenna_gama = np.zeros((self.size_sig, 3), dtype=np.complex64)
-        for axis in range(3):
-            [_, antenna_gama[:, axis]] = halfcplx_fullcplx_padding(
-                self.size_sig,
-                self.f_step,
-                self.f_start,
-                self.f_end,
-                s11_short[:, axis],
-            )
         z_ant = z_0 * (1 + antenna_gama) / (1 - antenna_gama)
         z_in_lna = z_0 * (1 + self.lna_gama) / (1 - self.lna_gama)
-        self.rho1 = z_in_lna / (z_ant + z_in_lna)
-        self.rho2 = (1 + self.lna_gama) / (1 - antenna_gama * self.lna_gama)
-        self.rho3 = self.lna_s21 / (1 + self.lna_gama)
-        self.rho123 = self.rho1 * self.rho2 * self.rho3
+        print(z_ant.shape)
+        print(z_in_lna.shape)
+        self._rho1 = z_in_lna / (z_ant + z_in_lna)
+        self._rho2 = (1 + self.lna_gama) / (1 - antenna_gama * self.lna_gama)
+        self._rho3 = self.lna_s21 / (1 + self.lna_gama)
+        self.rho123 = self._rho1 * self._rho2 * self._rho3
         self.z_ant = z_ant
         self.z_in_lna = z_in_lna
         self.antenna_gama = antenna_gama
+
+    def get_rho(self):
+        return self.rho123.T
 
     ### PLOT
 
     def plot_gama(self):  # pragma: no cover
         plt.figure()
-        plt.title("")
-        plt.plot(self.f_expan, np.abs(self.antenna_gama[:, 0]), label=r"")
-        # plt.plot(self.f_expan, np.abs(self.z_in_lna[:, 0]), label=r"$\mathregular{Z^{in}_{LNA}}$")
+        plt.title("antenna_gama")
+        plt.plot(self.freqs_out, np.abs(self.antenna_gama[:, 0]), label=r"0")
+        plt.plot(self.freqs_out, np.abs(self.antenna_gama[:, 1]), label=r"1")
+        plt.plot(self.freqs_out, np.abs(self.antenna_gama[:, 2]), label=r"2")
+        # plt.plot(self.freqs_out, np.abs(self.z_in_lna[:, 0]), label=r"$\mathregular{Z^{in}_{LNA}}$")
         plt.grid()
         plt.legend()
 
     def plot_z(self):  # pragma: no cover
         plt.figure()
         plt.title("")
-        plt.plot(self.f_expan, np.abs(self.z_ant[:, 0]), label=r"$\mathregular{Z_{A}}$")
-        plt.plot(self.f_expan, np.abs(self.z_in_lna[:, 0]), label=r"$\mathregular{Z^{in}_{LNA}}$")
+        plt.plot(self.freqs_out, np.abs(self.z_ant[:, 0]), label=r"$\mathregular{Z_{A}}$")
+        plt.plot(self.freqs_out, np.abs(self.z_in_lna[:, 0]), label=r"$\mathregular{Z^{in}_{LNA}}$")
         plt.grid()
         plt.legend()
 
@@ -312,7 +278,7 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
         plt.figure()
         # plt.rcParams["font.sans-serif"] = ["Times New Roman"]
         for p in range(3):
-            plt.plot(self.f_lna, self.dbs21_a[p])
+            plt.plot(self.f_lna, self._dbs21_a[p])
         plt.ylim(20, 25)
         plt.xlabel("Frequency(MHz)", fontsize=15)
         plt.ylabel(r"$\mathregular{S_{21}/dB} $", fontsize=15)
@@ -322,7 +288,7 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
         # plt.rcParams["font.sans-serif"] = ["Times New Roman"]
         plt.subplot(1, 3, 1)
         for p in range(3):
-            plt.plot(self.f_expan, abs(self.rho1[:, p]))
+            plt.plot(self.freqs_out, np.abs(self._rho1[:, p]))
         plt.legend(["port X", "port Y", "port Z"], loc="upper right")
         plt.xlabel("Frequency(MHz)", fontsize=15)
         plt.ylabel(r"$\mathregular{mag(\rho_1)}$", fontsize=15)
@@ -330,7 +296,7 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
         plt.title("the contribution of " + r"$\mathregular{ \rho_1}$")
         plt.subplot(1, 3, 2)
         for p in range(3):
-            plt.plot(self.f_expan, abs(self.rho2[:, p]))
+            plt.plot(self.freqs_out, np.abs(self._rho2[:, p]))
         plt.legend(["port X", "port Y", "port Z"], loc="upper right")
         plt.xlabel("Frequency(MHz)", fontsize=15)
         plt.ylabel(r"$\mathregular{mag(\rho_2)}$", fontsize=15)
@@ -338,7 +304,7 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
         plt.title("the contribution of " + r"$\mathregular{ \rho_2}$")
         plt.subplot(1, 3, 3)
         for p in range(3):
-            plt.plot(self.f_expan, abs(self.rho3[:, p]))
+            plt.plot(self.freqs_out, np.abs(self._rho3[:, p]))
         plt.legend(["port X", "port Y", "port Z"], loc="upper right")
         plt.xlabel("Frequency(MHz)", fontsize=15)
         plt.ylabel(r"$\mathregular{mag(\rho_3)}$", fontsize=15)
@@ -347,11 +313,33 @@ class LowNoiseAmplificatorGP300(GenericProcessingDU):
         plt.tight_layout()
         plt.subplots_adjust(top=0.85)
 
-class MasterRfChain():
+
+class LowNoiseAmplificatorGP300(LowNoiseAmplificator):
+    """!
+    @authors PengFei Zhang and Xidian group, GRANDLIB adaption Colley jm
+
+    Class goals:
+      * define data model used for GP300
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    ### INTERNAL
+
+    def _set_name_data_file(self, axis):
+        """!
+
+        @param axis:
+        """
+        lna_address = os.path.join("detector", "LNASparameter", f"{axis+1}.s2p")
+        return grand_add_path_data_model(lna_address)
+
+
+class MasterRfChain:
     """
     Facade for all detector effect
     """
-    
+
     def __init__(self):
         pass
-    
