4fd2ce02ec02ace17f81de8ad81edc1ddc03d4f2
luckyjim <jcolley@lpnhe.in2p3.fr>
2023-02-13 15:34:14 +0100

    remove TAB

--- env/_setup_lib.sh
+++ env/_setup_lib.sh
@@ -7,9 +7,9 @@ cd $GRAND_ROOT/src
 # test conda case, for amd64 
 if [ ! -z $CONDA_PREFIX ]
 then
-	echo "Add conda path to env variable C_INCLUDE_PATH and LIBRARY_PATH"
-    export C_INCLUDE_PATH=$C_INCLUDE_PATH:$CONDA_PREFIX/include
-    export LIBRARY_PATH=$LIBRARY_PATH:$CONDA_PREFIX/lib
+	echo "Add conda path to C_INCLUDE_PATH and LIBRARY_PATH"
+	export C_INCLUDE_PATH=$C_INCLUDE_PATH:$CONDA_PREFIX/include
+	export LIBRARY_PATH=$LIBRARY_PATH:$CONDA_PREFIX/lib
 fi   
 # 
 ./install_ext_lib.bash
