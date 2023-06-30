b9462b91ae26664a0fb2110ca31e4825d57387d7
lwpiotr <subs@lwp.email>
2023-06-30 16:30:27 +0200

    Fixed old traces use

--- examples/grandlib_classes/data_play.py
+++ examples/grandlib_classes/data_play.py
@@ -17,9 +17,9 @@ if len(sys.argv)==2:
     e.print()
     print(e.data_source)
     for i,v in enumerate(e.voltages):
-        print(i, np.array(v.trace_x))
+        print(i, np.array(v.trace)[:,0])
     for i,ef in enumerate(e.efields):
-        print(i, np.array(ef.trace_x))
+        print(i, np.array(ef.trace)[:,0])
 
     # Hard exit to avoid ROOT crash on defriending - not needed for ROOT 6.26.02 and above
     if ROOT.gROOT.GetVersionInt() < 62602:
