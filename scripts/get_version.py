3b908ad0fc9e8abcef7332f41c4e6f1f2e1413cc
LEGRAND Francois <francois.legrand@lpnhe.in2p3.fr>
2023-09-12 12:09:17 +0200

    Workflow to get version and tag

---
+++ scripts/get_version.py
@@ -0,0 +1,12 @@
+from os.path import exists
+versionfile = "grand/dataio/version"
+
+if not exists(versionfile):
+    version = "0.0.0"
+else:
+    f = open(versionfile, "r")
+    version = f.read()
+    f.close()
+
+
+print("version="+version)
