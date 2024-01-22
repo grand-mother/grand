be38366a60e0e60aa9ccfb10382b828560626adc
lwpiotr <subs@lwp.email>
2024-01-22 17:03:44 +0100

    Creating proper output directory and proper file names

--- sim2root/Common/sim2root.py
+++ sim2root/Common/sim2root.py
@@ -6,13 +6,21 @@ import os
 import argparse
 from types import SimpleNamespace
 import time
+from pathlib import Path
 from grand.dataio.root_trees import * # this is home/grand/grand (at least in docker) or ../../grand
 import raw_root_trees as RawTrees # this is here in Common
 
 
 # Command line argument parsing
-clparser = argparse.ArgumentParser()
+clparser = argparse.ArgumentParser(description="Convert simulation data in rawroot format into GRANDROOT format")
 clparser.add_argument("filename", nargs='+', help="ROOT file containing GRANDRaw data TTrees")
+clparser.add_argument("-o", "--output_parent_directory", help="Output parent directory", default="")
+clparser.add_argument("-fo", "--forced_output_directory", help="Force this option as the output directory", default=None)
+clparser.add_argument("-s", "--site_name", help="The name of the site", default="nosite")
+clparser.add_argument("-d", "--sim_date", help="The date of the simulation", default="19000101")
+clparser.add_argument("-e", "--extra", help="Extra information to store in the directory name", default="")
+clparser.add_argument("-av", "--analysis_level", help="Analysis level of the data", default=0, type=int)
+# clparser.add_argument("-se", "--serial", help="Serial number of the simulation", default=0)
 clparser.add_argument("-la", "--latitude", help="Latitude of the site", default=40.984558)
 clparser.add_argument("-lo", "--longitude", help="Longitude of the site", default=93.952247)
 clparser.add_argument("-al", "--altitude", help="Altitude of the site", default=1200)
@@ -32,27 +40,40 @@ def main():
     if clargs.start_event is not None:
         ext_event_number = int(clargs.start_event)
 
+    # Create the appropriate output directory
+    if clargs.forced_output_directory is None:
+        out_dir_name = form_directory_name(clargs)
+        print("Storing files in directory ", out_dir_name)
+        out_dir_name.mkdir()
+    # If another directory was forced as the output directory, create it
+    else:
+        out_dir_name = Path(clargs.output_parent_directory, clargs.forced_output_directory)
+        out_dir_name.mkdir(exist_ok=True)
+
+    start_event_number = 0
 
     # Loop through the files specified on command line
     for file_num, filename in enumerate(clargs.filename):
 
         # Output filename for GRAND Trees
-        # ToDo: think how to replace the original better
+        # if clargs.output_filename is None:
         out_filename = os.path.join(os.path.split(filename)[0], "gr_"+os.path.split(filename)[1])
+        # else:
+        #     out_filename = clargs.output_filename
 
         # Read the raw trees from the file
         trawshower = RawTrees.RawShowerTree(filename)
         trawefield = RawTrees.RawEfieldTree(filename)
         trawmeta = RawTrees.RawMetaTree(filename)
 
-        # Create appropriate GRANDROOT trees
+        # Create appropriate GRANDROOT trees in temporary file names (event range not known until the end of the loop)
         gt = SimpleNamespace()
-        gt.trun = TRun(out_filename)
-        gt.trunshowersim = TRunShowerSim(out_filename)
-        gt.trunefieldsim = TRunEfieldSim(out_filename)
-        gt.tshower = TShower(out_filename)
-        gt.tshowersim = TShowerSim(out_filename)
-        gt.tefield = TEfield(out_filename)
+        gt.trun = TRun((out_dir_name/"trun.root").as_posix())
+        gt.trunshowersim = TRunShowerSim((out_dir_name/"trunshowersim.root").as_posix())
+        gt.trunefieldsim = TRunEfieldSim((out_dir_name/"trunefieldsim.root").as_posix())
+        gt.tshower = TShower((out_dir_name/"tshower.root").as_posix())
+        gt.tshowersim = TShowerSim((out_dir_name/"tshowersim.root").as_posix())
+        gt.tefield = TEfield((out_dir_name/"tefield.root").as_posix())
 
         # Loop through entries - assuming same number of entries in each tree
         # ToDo: this should be a tree iterator through one tree and getting the other through friends. Need to make friends working...
@@ -108,6 +129,10 @@ def main():
                 gt.tshowersim.event_number = ext_event_number
                 gt.tefield.event_number = ext_event_number
 
+            # For the first file/iteration, store the event number
+            if file_num==0 and i==0:
+                start_event_number = gt.tshower.event_number
+
             # Fill the event trees
             gt.tshower.fill()
             gt.tshowersim.fill()
@@ -121,6 +146,11 @@ def main():
         # Increment the event number if starting one specified on command line
         if ext_event_number is not None:
             ext_event_number += 1
+
+    # Rename the created files to appropriate names
+    end_event_number = gt.tshower.event_number
+    print("Renaming files to proper file names")
+    rename_files(clargs, out_dir_name, start_event_number, end_event_number)
 
 
 # Convert the RawShowerTree first entry to run values
@@ -419,6 +449,41 @@ def rawmeta2grandroot(trawmeta, gt):
 def get_origin_geoid(clargs):
     origin_geoid = [clargs.latitude, clargs.longitude, clargs.altitude]
     return origin_geoid
+
+# Form the proper output directory name from command line arguments
+def form_directory_name(clargs):
+    # Change possible underscores in extra into -
+    extra = clargs.extra.replace("_", "-")
+
+    # Go through serial numbers in directory names to find a one that does not exist
+    for sn in range(1000):
+        dir_name = Path(clargs.output_parent_directory, f"sim_{clargs.site_name}_{clargs.sim_date}_{extra}_{sn:0>4}")
+        if not dir_name.exists():
+            break
+    # If directories with serial number up to 1000 already created
+    else:
+        print("All directories with serial number up to 1000 already exist. Please clean up some directories!")
+        exit(0)
+
+    return dir_name
+
+# Rename the created files to appropriate names
+def rename_files(clargs, path, start_event_number, end_event_number):
+    # Go through output files
+    for fn_start in ["trun", "trunshowersim", "trunefieldsim", "tshower", "tshowersim", "tefield"]:
+        # Go through serial numbers in directory names to find a one that does not exist
+        for sn in range(1000):
+            fn_in = Path(path, f"{fn_start}.root")
+            # Proper name of the file
+            fn_out = Path(path, f"{fn_start}_{start_event_number}-{end_event_number}_L{clargs.analysis_level}_{sn:0>4}.root")
+            # If the output file with the current serial number does not exist, rename to it
+            if not fn_out.exists():
+                fn_in.rename(fn_out)
+                break
+        else:
+            print(f"Could not find a free filename for {fn_in} until serial number 1000. Please clean up some files!")
+            exit(0)
+
 
 if __name__ == '__main__':
     main()
