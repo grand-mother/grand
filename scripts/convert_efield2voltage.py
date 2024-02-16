32e8ad4d8f86f541852beccd3bb0b36d1651111f
lwpiotr <subs@lwp.email>
2024-02-16 16:33:50 +0100

    Accepting only sim2root directory (structure) as input; added output directory

--- scripts/convert_efield2voltage.py
+++ scripts/convert_efield2voltage.py
@@ -57,10 +57,15 @@ def manage_args():
         description="Calculation of DU response in volt for first event in Efield input file."
     )
     parser.add_argument(
-        "file",
-        help="Efield input data file in GRANDROOT format.",
-        type=argparse.FileType("r"),
+        "directory",
+        help="Simulation output data directory in GRANDROOT format.",
+        # type=argparse.FileType("r"),
     )
+    # parser.add_argument(
+    #     "file",
+    #     help="Efield input data file in GRANDROOT format.",
+    #     type=argparse.FileType("r"),
+    # )
     parser.add_argument(
         "--no_noise",
         action="store_false",
@@ -76,11 +81,17 @@ def manage_args():
     parser.add_argument(
         "-o",
         "--out_file",
-        default="",
+        default=None,
         help="output file in GRANDROOT format. If the file exists it is overwritten.",
-        required=True,
+        # required=True,
         # PB with option ???
         # type=argparse.FileType("w"),
+    )
+    parser.add_argument(
+        "-od",
+        "--out_directory",
+        default=None,
+        help="output directory in GRANDROOT format. If not given, is it the same as input directory",
     )
     parser.add_argument(
         "--verbose",
@@ -136,6 +147,11 @@ if __name__ == "__main__":
     logger.info("Computing voltage from the input electric field.")
 
     args = manage_args()
+
+    # If no output directory given, define it as input directory
+    if args.out_directory is None:
+        args.out_directory = args.directory
+
     # define a handler for logger : standard only
     mlg.create_output_for_logger(args.verbose, log_stdout=True)
     logger.info(mlg.string_begin_script())
@@ -143,7 +159,8 @@ if __name__ == "__main__":
     seed = None if args.seed==-1 else args.seed
     logger.info(f"seed used for random number generator is {seed}.")
 
-    signal = Efield2Voltage(args.file.name, args.out_file, seed=seed, padding_factor=args.padding_factor)
+    # signal = Efield2Voltage(args.file.name, args.out_file, seed=seed, padding_factor=args.padding_factor)
+    signal = Efield2Voltage(args.directory, args.out_file, output_directory=args.out_directory, seed=seed, padding_factor=args.padding_factor)
     signal.params["add_noise"]    = args.no_noise
     signal.params["add_rf_chain"] = args.no_rf_chain
     signal.params["lst"]          = args.lst
