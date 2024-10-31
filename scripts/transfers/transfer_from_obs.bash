24290069b68e4909442c0f80ed9334bf4c69d8cc
LEGRAND Francois <francois.legrand@lpnhe.in2p3.fr>
2024-10-31 17:26:42 +0100

    Unified script to be distributed on all sites. Include a purge of the already transfered files from the database before transfer.

--- scripts/transfers/transfer_from_obs.bash
+++ scripts/transfers/transfer_from_obs.bash
@@ -6,24 +6,39 @@
 ##### Configuration part #####
 # Please adjust the following variable to your site
 
-# Local database name (sqlite filename)
-# '/home/grand/data-transfer/grand_transfer.db' for gaa
-#
-dbfile='grand_transfer.db'
-
-# Local directory where are stored the data to be transfered (will be explored recursively)
-# '/home/mapx/mapx/DunhuangData/Rawdata/20dB/' for gp13
-# '/home/grand/Malargue/' for gaa
-localdatadir='/home/mapx/mapx/DunhuangData/Rawdata/20dB/'
-
-#path to local rsync to use (leave blank if your default rsync --version >= 3.2.3
-# define the path to rsync version >= 3.2.3 otherwise
-rsyncpath=''
-
 # Site name prefix in filenames
 # 'GP13' for gp13
 # gaa for gaa
 site='GP13'
+
+# Start date for transfer (all files older than this date will be skipped
+first_transfer='20240314'
+
+# skip open files (if set to false the opened files will be transfered into /tmp)
+declare -A defskipopenfiles
+defskipopenfiles["gaa"]=false
+defskipopenfiles["GP13"]=true
+skipopenfiles=${defskipopenfiles[$site]}
+
+# Local database name (sqlite filename)
+# '/home/grand/data-transfer/grand_transfer.db' for gaa
+#
+declare -A defdbfile
+defdbfile["gaa"]='/home/grand/data-transfer/grand_transfer.db'
+defdbfile["GP13"]='grand_transfer_Since202409.db'
+dbfile=${defdbfile[$site]}
+
+# Local directory where are stored the data to be transfered (will be explored recursively)
+# '/home/mapx/mapx/DunhuangData/Rawdata/20dB/' for gp13
+# '/home/grand/Malargue/' for gaa
+declare -A deflocaldatadir
+deflocaldatadir["gaa"]='/data/Malargue/'
+deflocaldatadir["GP13"]='/share03/users/m/mapx/cc-in2p3/'
+localdatadir=${deflocaldatadir[$site]}
+
+#path to local rsync to use (leave blank if your default rsync --version >= 3.2.3
+# define the path to rsync version >= 3.2.3 otherwise
+rsyncpath=''
 
 # Remote server to transfer
 remote_server='cca.in2p3.fr'
@@ -34,30 +49,42 @@ remote_account='prod_grand'
 #ssh key for rsync
 # "/home/mapx/.ssh/id_ed25519" for gp13
 # "/root/.ssh/id_ed25519-nopw" for gaa
-ssh_key_rsync="/home/mapx/.ssh/id_ed25519"
+declare -A defssh_key_rsync
+defssh_key_rsync["gaa"]="/root/.ssh/id_ed25519-nopw"
+defssh_key_rsync["GP13"]="/home/mapx/.ssh/id_ed25519-rsync"
+ssh_key_rsync=${defssh_key_rsync[$site]}
 
 #ssh key for exec remote scripts
 # "/home/mapx/.ssh/id_ed25519-scrips" for gp13
 # "/root/.ssh/id_ed25519-nopw-scripts" for gaa
-ssh_key_exec="/home/mapx/.ssh/id_ed25519-scrips"
+declare -A defssh_key_exec
+defssh_key_exec["gaa"]="/root/.ssh/id_ed25519-nopw-scripts"
+defssh_key_exec["GP13"]="/home/mapx/.ssh/id_ed25519-scripts"
+ssh_key_exec=${defssh_key_exec[$site]}
 
 # Target directory on remote server, must be the same directory as the one defined in remote_account@remote_server:~/.ssh/authorized_keys for ssh_key_rsync
 # '/sps/grand/data/gp13' for gp13
 # '/sps/grand/data/gaa' for gaa
-remotedatadir='/sps/grand/data/gp13' #'/sps/grand/prod_grand/tests'  #'/sps/grand/data/gp13'
+declare -A defremotedatadir
+defremotedatadir["gaa"]='/sps/grand/data/gaa'
+defremotedatadir["GP13"]='/sps/grand/data/gp13'
+remotedatadir=${defremotedatadir[$site]}
 
-# Start date for transfer (all files older than this date will be skipped
-first_transfer='20240314'
 
 # Local script to be launched before run
 # '' for gp13
 #'setup_network_auger.bash -init' for gaa
-pre_run_script=''
+declare -A defpre_run_script
+defpre_run_script["gaa"]='/root/bin/setup_network_auger.bash -init'
+defpre_run_script["GP13"]=''
+pre_run_script=${defpre_run_script[$site]}
 
 # Local script to be launched after run
 # '' for gp13
 # '/root/bin/setup_network_auger.bash -close' for gaa
-post_run_script=''
+declare -A defpost_run_script
+defpost_run_script["gaa"]='/root/bin/setup_network_auger.bash -close'
+defpost_run_script["GP13"]=''
 
 # rsync_options : a to keep the creation time of files, z to compress if bandwidth is limited (but it's ~5 times slower). Please keep the "a" option  !
 rsync_options="-az --mkpath --chmod=go-w"
@@ -91,7 +118,7 @@ last_transfer=$(( last_transfer > first_transfer ? last_transfer : first_transfe
 
 #tag to identify files treated in the current run
 tag=$(date +'%Y%m%d%H%M%S')
-dbname=$(basename dbfile)
+
 
 # Colors
 Default='\033[0m'	# Text Reset
@@ -114,6 +141,7 @@ declare -A toins=([1]="")
 i=1
 j=0
 md5="0"
+declare -a openfiles=() ##tmp
 #find all files in localdatadir corresponding to datas (i.e. name starting by site) and add them to the database if not here already
 for file in $(find $localdatadir -type f -newermt $last_transfer| grep /${site}_ |sort)
 do
@@ -131,6 +159,8 @@ do
     fi
     toins[$i]+=",('$(dirname $file)', '$(basename $file)', ${dateobs}, 0, '${md5}')"
     j=$((j + 1))
+  else
+    openfiles+=("$file") ##tmp
   fi
 done
 
@@ -169,12 +199,21 @@ do
 	  #echo $md52
 		#Transfer successful : store info to update database at the end
 		translog[$i]+=";UPDATE gfiles SET success=1, md5sum='${md5}' WHERE id=${fileinfo[4]};INSERT INTO transfer (id, tag, success,date_transfer,target,comment) VALUES (${fileinfo[4]},${tag}, 1,datetime('now','utc'), \"${remotedatadir}/raw/${fileinfo[2]:0:4}/${fileinfo[2]:4:2}/${finalname}\", '${trans}')"
-		printf "${Green}Ok${Default}"
+		# colors only for terminal
+		if [ -n "$TERM" ]; then
+		  printf "${Green}Ok${Default}"
+		else
+		  printf "Ok"
+		fi
 	else
 	  md5=$(echo ${trans}|awk -F"md5:" '{print $2}')
 		#Transfer failed : just log errors
 		translog[$i]+=";INSERT INTO transfer (id, tag, success, date_transfer, target, comment) VALUES (${fileinfo[4]}, ${tag}, 0,datetime('now','utc'), '${remotedatadir}/raw/${fileinfo[2]:0:4}/${fileinfo[2]:4:2}/${finalname}', '${trans}')"
-		printf "${Red}ERROR:${Default} \n ${trans} "
+		if [ -n "$TERM" ]; then
+		  printf "${Red}ERROR:${Default} \n ${trans} "
+		else
+		  printf "ERROR: \n ${trans} "
+		fi
 	fi
 
 	# split info to store into db in case of large number of files
@@ -200,10 +239,36 @@ for key in "${!translog[@]}"; do
 done
 
 #finally also rsync the database
+# To avoid to transfer the whole database, we will purge the database from old files (already transfered)
+# Get the list of files successfully transfered before the last transfer
+list_id=$(sqlite3 $dbfile "select id from gfiles where success==1 and date<$last_transfer")
+# Convert it into an array
+ids=()
+for id in $list_id; do ids+=($id) ; done
+# Perform the deletion by groups of 500 files
+CHUNK_SIZE=500
+index=0
+while [[ $index -lt ${#ids[@]} ]]; do
+        nids=$(echo "${ids[@]:$index:$CHUNK_SIZE}" | tr ' ' ',')
+        sqlite3 $dbfile "delete from transfer where id in ($nids); delete from gfiles where id in ($nids)"
+        index=$((index + CHUNK_SIZE))
+done
+# finally create a new DB with only the last files. We set the remote db name to ${tag}_${site}_dbfile.db
+dirdb=$(dirname $dbfile)
+dbname="${tag}_${site}_dbfile.db"
+
+sqlite3 $dbfile "vacuum main into '${dirdb}/${dbname}'"
+
 # no remotedatadir specified on rsync command line, as it's already the defined restricted directory in ccin2p3:~/.ssh/authorized_keys
 # /raw directory in the rsync thus has /sps/grand/data/gaa as root dir
-${rsync_command} -e "ssh ${ssh_options}" ${rsync_options} $dbfile ${remote_account}@${remote_server}:/logs/${tag}_${dbname}
-#rsync -e "ssh ${ssh_options}" ${rsync_options} --rsync-path="mkdir -p $remotedatadir/logs && rsync" $dbfile ${remote_account}@${remote_server}:$remotedatadir/logs/${tag}_${dbname}
+${rsync_command} -e "ssh ${ssh_options}" ${rsync_options} ${dirdb}/${dbname} ${remote_account}@${remote_server}:/logs/${dbname}
+
+# sync openfiles into tmp ##tmp
+if [ "$skipopenfiles" = false ]; then
+  for file in ${openfiles[*]}; do
+	  ${rsync_command} -v -e "ssh ${ssh_options}" ${rsync_options} $file ${remote_account}@${remote_server}:/tmp/
+  done
+fi
 
 #close ssh connection
 ssh -O exit $ssh_options ${remote_account}@${remote_server}
@@ -212,7 +277,7 @@ rm -rf ~/.ssh/ctl
 #Run conversion scripts @ccin2p3
 if [ -n "$ccscripts" ]
 then
-  ssh -i ${ssh_key_exec} ${remote_account}@${remote_server} ${ccscripts} -s ${site} -d ${remotedatadir}/logs/${tag}_${dbname} -t ${tag}
+  ssh -i ${ssh_key_exec} ${remote_account}@${remote_server} ${ccscripts} -s ${site} -d ${remotedatadir}/logs/${dbname} -t ${tag}
 fi
 
 # run post script
