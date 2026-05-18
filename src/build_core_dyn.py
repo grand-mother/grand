d904793367a37240ce607e7155f09ab81f873f50
Maxime Regeard <regeard@apc.in2p3.fr>
2026-05-18 16:43:45 +0200

    dynamic library cffi build script

---
+++ src/build_core_dyn.py
@@ -0,0 +1,94 @@
+from cffi import FFI
+import os
+from pathlib import Path
+import platform
+from pycparser import parse_file, c_generator
+import sys
+import subprocess
+import sysconfig
+
+
+SRC_DIR = Path(__file__).parent.resolve()
+try:
+    BUILD_DIR = sys.argv[1]
+except IndexError:
+    BUILD_DIR = "build"
+else:
+    if BUILD_DIR == "bdist_wheel":
+        BUILD_DIR = "build-release"
+BUILD_DIR = Path(BUILD_DIR).resolve()
+
+LIB_DIR = BUILD_DIR / "lib"
+INC_DIR = BUILD_DIR / "include"
+TMP_DIR = BUILD_DIR / "tmp"
+PACKAGE_PATH = BUILD_DIR / "grand"
+
+
+ffi = FFI()
+
+
+def include(path, **opts):
+    args = []
+    for k, v in opts.items():
+        args.append(f"-D{k}={v}")
+    args.append("-I" + str(INC_DIR))
+    args.append("-DFILE=struct FILE")
+
+    ast = parse_file(str(path), use_cpp=True, cpp_args=args)
+    generator = c_generator.CGenerator()
+    header = generator.visit(ast)
+    ffi.cdef(header)
+
+
+include(SRC_DIR / "grand.h")
+
+
+def configure():
+    if platform.system() == "Darwin":
+        rpath = rpath = ["-Wl,-rpath,@loader_path/../lib"]
+    else:
+        rpath = ["-Wl,-rpath,$ORIGIN/../lib"]
+
+    with open(SRC_DIR / "grand.c") as f:
+        ffi.set_source(
+            "grand._core",
+            f.read(),
+            libraries=["turtle", "gull"],
+            include_dirs=[str(INC_DIR), str(SRC_DIR)],
+            library_dirs=[str(LIB_DIR)],
+            extra_link_args=rpath,
+        )
+
+
+configure()
+
+
+def build():
+    TMP_DIR.mkdir(parents=True, exist_ok=True)
+    PACKAGE_PATH.mkdir(parents=True, exist_ok=True)
+
+    os.chdir(TMP_DIR)
+    module = Path(ffi.compile(verbose=False))
+    module = module.rename(PACKAGE_PATH / "_core.abi3.so")
+
+    # Strip conda rpath
+    conda_lib = str(Path(sysconfig.get_path("stdlib")).parent.parent / "lib")
+
+    if platform.system() == "Darwin":
+        # Remove duplicate rpaths on macOS
+        result = subprocess.run(
+            ["otool", "-l", str(module)], capture_output=True, text=True
+        )
+        seen = False
+        for line in result.stdout.splitlines():
+            if "path" in line and conda_lib in line:
+                if seen:
+                    subprocess.run(
+                        ["install_name_tool", "-delete_rpath", conda_lib, str(module)]
+                    )
+                else:
+                    seen = True  # keep first occurrence, delete the rest
+
+
+if __name__ == "__main__":
+    build()
